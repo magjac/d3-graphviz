ec3075d2cb3671f2676731462a1602bd43ed5c24
Magnus Jacobsson <magjac@netinsight.net>
2017-07-03 17:27:33 +0200

    Added rendering of SVG from DOT

--- src/render.js
+++ src/render.js
@@ -1,3 +1,16 @@
+import * as Viz from "viz.js";
+import * as d3 from "d3-selection";
+
 export default function(src, element) {
+
+    var svg = Viz(src,
+              {
+                  format: "svg",
+                  engine: "dot"
+              }
+             );
+
+    d3.select(element)
+        .html(svg);
     return 42;
 };
--- test/graphviz-test.js
+++ test/graphviz-test.js
@@ -1,7 +1,7 @@
 var tape = require("tape"),
     graphviz = require("../");
 
-tape("graphviz() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-    test.equal(graphviz.render(), 42);
+tape("graphviz() renders an SVG from graphviz DOT.", function(test) {
+    test.equal(graphviz.render('digraph {a -> b;}'), 42);
     test.end();
 });
