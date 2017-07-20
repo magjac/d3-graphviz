77b8693ab2962940417b7e11d9f6b896458c6a8c
Magnus Jacobsson <magjac@netinsight.net>
2017-07-20 10:51:09 +0200

    Added createElementWithAttributes function

--- src/element.js
+++ src/element.js
@@ -1,3 +1,5 @@
+import * as d3 from "d3-selection";
+
 export function extractElementData(element) {
 
     var datum = {};
@@ -30,4 +32,18 @@ export function createElement(data) {
     } else {
         return document.createElementNS('http://www.w3.org/2000/svg', data.tag);
     }
+}
+
+export function createElementWithAttributes(data) {
+
+    var elementNode = createElement(data);
+    var element = d3.select(elementNode);
+    var attributes = data.attributes;
+    if (attributes) {
+        for (var attributeName of Object.keys(attributes)) {
+            var attributeValue = attributes[attributeName];
+            element.attr(attributeName, attributeValue);
+        }
+    }
+    return elementNode;
 }
