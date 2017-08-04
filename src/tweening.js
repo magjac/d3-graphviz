6b14a39b86ed2cc676792cc04b4a1bbe00a5c3f5
Magnus Jacobsson <magjac@netinsight.net>
2017-08-04 12:58:35 +0200

    Refactor: moved path points calculation to new function

--- src/tweening.js
+++ src/tweening.js
@@ -1,24 +1,28 @@
 export function pathTween(d1, precision) {
     return function() {
-        var path0 = this,
-            path1 = path0.cloneNode(),
-            n0 = path0.getTotalLength(),
-            n1 = (path1.setAttribute("d", d1), path1).getTotalLength();
-
-        // Uniform sampling of distance based on specified precision.
-        var distances = [0], i = 0, dt = precision / Math.max(n0, n1);
-        while ((i += dt) < 1) distances.push(i);
-        distances.push(1);
-
-        // Compute point-interpolators at each distance.
-        var points = distances.map(function(t) {
-            var p0 = path0.getPointAtLength(t * n0),
-                p1 = path1.getPointAtLength(t * n1);
-            return d3.interpolate([p0.x, p0.y], [p1.x, p1.y]);
-        });
-
+        var points = pathTweenPoints(this, d1, precision);
         return function(t) {
             return t < 1 ? "M" + points.map(function(p) { return p(t); }).join("L") : d1;
         };
     };
+}
+
+export function pathTweenPoints(node, d1, precision) {
+    var path0 = node,
+        path1 = path0.cloneNode(),
+        n0 = path0.getTotalLength(),
+        n1 = (path1.setAttribute("d", d1), path1).getTotalLength();
+
+    // Uniform sampling of distance based on specified precision.
+    var distances = [0], i = 0, dt = precision / Math.max(n0, n1);
+    while ((i += dt) < 1) distances.push(i);
+    distances.push(1);
+
+    // Compute point-interpolators at each distance.
+    var points = distances.map(function(t) {
+        var p0 = path0.getPointAtLength(t * n0),
+            p1 = path1.getPointAtLength(t * n1);
+        return d3.interpolate([p0.x, p0.y], [p1.x, p1.y]);
+    });
+    return points;
 }
