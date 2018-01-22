017aabb22b2f623ecaab87a126181d67ea6113d2
Magnus Jacobsson <magjac@netinsight.net>
2018-01-22 06:52:52 +0100

    Added check that the events are registered in logEvents test

--- test/logEvents-test.js
+++ test/logEvents-test.js
@@ -13,5 +13,14 @@ tape("logEvents(true) enables event logging.", function(test) {
         .dot('digraph {a -> b;}')
         .render();
 
+    var eventTypes = graphviz._eventTypes;
+    n = 0;
+    for (let i in eventTypes) {
+        let eventType = eventTypes[i];
+        test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
+        n += 1;
+    }
+    test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
+    test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
     test.end();
 });
