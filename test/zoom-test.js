97071d9128b365ca417cab6554b33a86a614cdc6
Magnus Jacobsson <magjac@netinsight.net>
2018-01-23 18:15:22 +0100

    Added zoom test

---
+++ test/zoom-test.js
@@ -0,0 +1,40 @@
+var tape = require("tape");
+var jsdom = require("./jsdom");
+var d3_graphviz = require("../");
+
+tape("zoom(false) disables zooming.", function(test) {
+    var window = global.window = jsdom('<div id="graph"></div>');
+    var document = global.document = window.document;
+    var graphviz = d3_graphviz.graphviz("#graph");
+    graphviz
+        .zoom(false)
+        .renderDot('digraph {a -> b;}');
+
+    test.notOk(graphviz._zoom, '.zoom(false) disables zooming');
+
+    test.end();
+});
+
+tape("zoom(true) enables zooming.", function(test) {
+    var window = global.window = jsdom('<div id="graph"></div>');
+    var document = global.document = window.document;
+    var graphviz = d3_graphviz.graphviz("#graph");
+
+// Workaround for error in d3-zoom.js:
+//   if (e instanceof SVGElement) {
+//                    ^
+// ReferenceError: SVGElement is not defined
+    global.SVGElement = function(){};
+    graphviz
+        .zoom(true);
+
+    test.ok(graphviz._zoom, '.zoom(true) enables zooming');
+    test.notOk(graphviz._zoomBehavior, 'The zoom behavior is not attached before a graph has been rendered');
+
+    graphviz
+        .renderDot('digraph {a -> b;}');
+
+    test.ok(graphviz._zoomBehavior, 'The zoom behavior is attached when the graph has been rendered');
+
+    test.end();
+});
