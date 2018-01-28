29a1ad2bdf312713a09839dc9d98fbed769809f3
Magnus Jacobsson <magjac@netinsight.net>
2018-01-28 08:51:48 +0100

    Added polyfill for jsdom SVGElement transform property

--- test/zoom-test.js
+++ test/zoom-test.js
@@ -26,6 +26,42 @@ function polyfillSVGElement() {
             }
         });
     }
+    if (!('transform' in window.SVGElement.prototype)) {
+        Object.defineProperty(window.SVGElement.prototype, 'transform', {
+            get: function() {
+                if (this.getAttribute('transform')) {
+                    var translate = this.getAttribute('transform').replace(/.*translate\((\d+[ ,]+\d+)\).*/, function(match, xy) {
+                        return xy;
+                    }).split(/[ ,]+/).map(function(v) {
+                        return +v;
+                    });
+                    var scale = this.getAttribute('transform').replace(/.*.*scale\((\d+[ ,]*\d*)\).*/, function(match, scale) {
+                        return scale;
+                    }).split(/[ ,]+/).map(function(v) {
+                        return +v;
+                    });
+                    return {
+                        baseVal: {
+                            numberOfItems: 1,
+                            consolidate: function() {
+                                return {
+                                    matrix: {
+                                        'a': scale[0],
+                                        'b': 0,
+                                        'c': 0,
+                                        'd': scale[1] || scale[0],
+                                        'e': translate[0],
+                                        'f': translate[1],
+                                    }
+                                };
+                            },
+                        },
+                    };
+                } else {
+                    return null;
+                }
+            },
+        });
     }
     global.SVGElement = window.SVGElement;
 }
