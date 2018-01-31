6c73a28bd8ee832a267453fcc60b6d7f600ebb52
Magnus Jacobsson <magjac@netinsight.net>
2018-02-01 00:09:35 +0100

    Added test of event logging also during transition

--- test/logEvents-test.js
+++ test/logEvents-test.js
@@ -1,6 +1,7 @@
 var tape = require("tape");
 var jsdom = require("./jsdom");
 var d3_graphviz = require("../");
+var d3_transition = require("d3-transition");
 
 tape("logEvents enables and disables event logging.", function(test) {
     var window = global.window = jsdom('<div id="graph"></div>');
@@ -37,5 +38,27 @@ tape("logEvents enables and disables event logging.", function(test) {
     }
     test.ok(n > 10, "None of the more than 10 events are registered when event logging is disabled");
     test.equal(n, eventTypes.length, "None of the " + eventTypes.length + " events are registered when event logging is disabled");
-    test.end();
+
+    graphviz
+        .zoom(false)
+        .logEvents(true)
+        .transition(function() {
+            return d3_transition.transition().duration(0);
+        })
+        .dot('digraph {a -> b;}')
+        .render()
+        .on("end", function () {
+
+            var eventTypes = graphviz._eventTypes;
+            n = 0;
+            for (let i in eventTypes) {
+                let eventType = eventTypes[i];
+                test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled and a transition is used");
+                n += 1;
+            }
+            test.ok(n > 10, "More than 10 events are registered when event logging is enabled and a transition is used");
+            test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled and a transition is used");
+            test.end();
+        });
+
 });
