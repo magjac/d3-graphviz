460b3fa128d30a3630c90ab6aac475bf94514b88
Magnus Jacobsson <magjac@netinsight.net>
2018-01-31 06:24:08 +0100

    Refactor: Moved the tweenPaths tests to a separate file

--- test/graphviz-test.js
+++ test/graphviz-test.js
@@ -248,48 +248,6 @@ tape("graphviz().render() changes SVG element type when node shape changes in DO
     test.end();
 });
 
-tape("graphviz().tweenPaths() enables and disables path tweening during transitions. FIXME: tape bug prohibits tweenPaths enabled test.", function(test) {
-
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph");
-
-    graphviz
-        .tweenShapes(false)
-        .zoom(false)
-        .dot('digraph {a -> b; c}')
-        .render();
-    test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
-    test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-    test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-    test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
-    test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
-    transition1 = d3_transition.transition().duration(0);
-    graphviz
-        .dot('digraph {a -> b; b -> a}')
-        .transition(transition1)
-        .fade(false)
-        .tweenPaths(false)
-        .render()
-        .on("end", part1_end);
-    test.equal(d3.selectAll('.node').size(), 3, 'Number of nodes immediately after rendering');
-    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
-    test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons immediately after rendering');
-    test.equal(d3.selectAll('ellipse').size(), 3, 'Number of ellipses immediately after rendering');
-    test.equal(d3.selectAll('path').size(), 2, 'Number of paths immediately after rendering');
-
-    function part1_end() {
-
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after transition');
-        test.equal(d3.selectAll('.edge').size(), 2, 'Number of edges after transition');
-        test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after transition');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses after transition');
-        test.equal(d3.selectAll('path').size(), 2, 'Number of paths after transition');
-
-        test.end();
-    }
-});
-
 tape("graphviz().tweenShapes() enables and disables shape tweening during transitions.", function(test) {
 
     var window = global.window = jsdom('<div id="graph"></div>');
