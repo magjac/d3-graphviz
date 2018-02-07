e8bd64acbcaf8d468129a0aeaeb94eb02dd61e42
Magnus Jacobsson <magjac@netinsight.net>
2018-02-07 07:02:20 +0100

    Test shape tween between non-equal sided polygons
    when conversion of equal sided polygons is disabled.

--- test/tweenShapes-test.js
+++ test/tweenShapes-test.js
@@ -42,6 +42,28 @@ tape("graphviz().tweenShapes() enables and disables shape tweening during transi
         test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after shape change');
         test.equal(d3.selectAll('path').size(), 1, 'Number of paths after shape change');
 
+        transition1 = d3_transition.transition().duration(0);
+        graphviz
+            .dot('digraph {node [shape="triangle"];a -> b}')
+            .transition(transition1)
+            .render()
+            .on ("end", part2_end);
+
+        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes immediately after rendering');
+        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
+        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses immediately after rendering');
+        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons immediately after rendering');
+        test.equal(d3.selectAll('path').size(), 3, 'Number of paths immediately after rendering');
+    }
+
+    function part2_end() {
+
+        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after shape change');
+        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges after shape change');
+        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after shape change');
+        test.equal(d3.selectAll('polygon').size(), 4, 'Number of polygons after shape change');
+        test.equal(d3.selectAll('path').size(), 1, 'Number of paths after shape change');
+
         test.end();
     }
 });
