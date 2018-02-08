e81de9212ecfc7c607363b02377536257df07eb3
Magnus Jacobsson <magjac@netinsight.net>
2018-02-09 00:10:02 +0100

    Removed unused stuff
    This particular stuff is unused since there is no need to check if the
    data for the element to be created contains attributes, because the
    createElementWithAttributes functions is always called for elements to
    be shape tweened, which is always polygons, ellipses or paths which
    all have attributes.

--- src/element.js
+++ src/element.js
@@ -73,11 +73,9 @@ export function createElementWithAttributes(data) {
     var elementNode = createElement(data);
     var element = d3.select(elementNode);
     var attributes = data.attributes;
-    if (attributes) {
-        for (var attributeName of Object.keys(attributes)) {
-            var attributeValue = attributes[attributeName];
-            element.attr(attributeName, attributeValue);
-        }
+    for (var attributeName of Object.keys(attributes)) {
+        var attributeValue = attributes[attributeName];
+        element.attr(attributeName, attributeValue);
     }
     return elementNode;
 }
