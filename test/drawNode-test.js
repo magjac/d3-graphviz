96e80a51ea4dba4da4cbd9edfac8cece59bee048
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-03-26 00:22:02 +0200

    Test drawing of polygon shaped nodes

--- test/drawNode-test.js
+++ test/drawNode-test.js
@@ -90,6 +90,55 @@ tape("drawNode() draws a node in the same way as Graphviz does", function(test) 
 
 });
 
+tape("drawNode() draws a polygon node", function(test) {
+    var window = global.window = jsdom('<div id="graph"></div>');
+    var document = global.document = window.document;
+    var graphviz = d3_graphviz.graphviz("#graph");
+
+    var num_nodes = 2;
+    var num_edges = 1;
+
+    graphviz
+        .zoom(false)
+        .logEvents(true)
+        .dot('digraph {graph [rankdir="LR"]; node [shape="polygon"]; a -> b;}')
+        .render(drawNode);
+
+    function drawNode() {
+        test.equal(d3.selectAll('.node').size(), num_nodes, 'Number of initial nodes');
+        test.equal(d3.selectAll('.edge').size(), num_edges, 'Number of initial edges');
+        test.equal(d3.selectAll('polygon').size(), num_edges + 1 + num_nodes, 'Number of initial polygons');
+        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of initial ellipses');
+        test.equal(d3.selectAll('path').size(), num_edges, 'Number of initial paths');
+
+        graphviz
+            .drawNode(0, -36, 54, 36, 'c', 'polygon');
+
+        num_nodes += 1;
+        test.equal(d3.selectAll('.node').size(), num_nodes, 'Number of nodes after drawing an edge');
+        test.equal(d3.selectAll('.edge').size(), num_edges, 'Number of edges after drawing an edge');
+        test.equal(d3.selectAll('polygon').size(), 1 + num_edges + num_nodes, 'Number of polygons after drawing an edge');
+        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after drawing an edge');
+        test.equal(d3.selectAll('path').size(), num_edges, 'Number of paths after drawing an edge');
+        graphviz
+            .insertCurrentNode('c');
+        test.equal(d3.selectAll('.node').size(), num_nodes, 'Number of nodes after inserting the currently drawn edge');
+        test.equal(d3.selectAll('.edge').size(), num_edges, 'Number of edges after inserting the currently drawn edge');
+        test.equal(d3.selectAll('polygon').size(), 1 + num_edges + num_nodes, 'Number of polygons after inserting the currently drawn edge');
+        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after inserting the currently drawn edge');
+        test.equal(d3.selectAll('path').size(), num_edges, 'Number of paths after inserting the currently drawn edge');
+
+        nodeShape = d3.selectAll('.node').selectAll('polygon').filter(function(d) {
+            return d.parent.key == 'c'
+        });
+
+        test.equal(nodeShape.attr("points"), '54,-36 0,-36 0,0 54,0');
+
+        test.end();
+    }
+
+});
+
 tape("drawNode() draws a node with an URL attribute in the same way as Graphviz does", function(test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
