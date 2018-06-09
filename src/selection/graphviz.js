7377d653384b83016a6bd4c22cc937fa175cffef
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-06-09 22:27:18 +0200

    Return an exiting graphviz renderer instead of creating a new
    Closes https://github.com/magjac/d3-graphviz/issues/63

--- src/selection/graphviz.js
+++ src/selection/graphviz.js
@@ -2,5 +2,12 @@ import {Graphviz} from "../graphviz";
 
 export default function(options) {
 
-    return new Graphviz(this, options);
+    var g = this.node().__graphviz__;
+    if (g) {
+        g.options(options);
+        g._dispatch.call("initEnd", this);
+    } else {
+        g = new Graphviz(this, options);
+    }
+    return g;
 }
