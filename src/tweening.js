895afb9f46b33f055273e031617c0ed825204534
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2018-09-16 08:18:12 +0200

    Refactor: tweening: One stmt per line. Braces. Use const & let

--- src/tweening.js
+++ src/tweening.js
@@ -2,7 +2,7 @@ import {interpolate} from "d3-interpolate";
 
 export function pathTween(points, d1) {
     return function() {
-        var pointInterpolators = points.map(function(p) {
+        const pointInterpolators = points.map(function(p) {
             return interpolate([p[0][0], p[0][1]], [p[1][0], p[1][1]]);
         });
         return function(t) {
@@ -12,20 +12,24 @@ export function pathTween(points, d1) {
 }
 
 export function pathTweenPoints(node, d1, precision) {
-    var path0 = node;
-    var path1 = path0.cloneNode();
-    var n0 = path0.getTotalLength();
-    var n1 = (path1.setAttribute("d", d1), path1).getTotalLength();
+    const path0 = node;
+    const path1 = path0.cloneNode();
+    const n0 = path0.getTotalLength();
+    const n1 = (path1.setAttribute("d", d1), path1).getTotalLength();
 
     // Uniform sampling of distance based on specified precision.
-    var distances = [0], i = 0, dt = precision / Math.max(n0, n1);
-    while ((i += dt) < 1) distances.push(i);
+    const distances = [0];
+    let i = 0;
+    const dt = precision / Math.max(n0, n1);
+    while ((i += dt) < 1) {
+      distances.push(i);
+    }
     distances.push(1);
 
     // Compute point-interpolators at each distance.
-    var points = distances.map(function(t) {
-        var p0 = path0.getPointAtLength(t * n0);
-        var p1 = path1.getPointAtLength(t * n1);
+    const points = distances.map(function(t) {
+        const p0 = path0.getPointAtLength(t * n0);
+        const p1 = path1.getPointAtLength(t * n1);
         return ([[p0.x, p0.y], [p1.x, p1.y]]);
     });
     return points;
