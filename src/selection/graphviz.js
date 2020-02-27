b218359053aa0d7d20ff02e7e40d40b745d958d8
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-27 17:35:02 +0100

    Fix old 'initEnd' event handler called when re-creating renderer instance
    Fixes https://github.com/magjac/d3-graphviz/issues/123

--- src/selection/graphviz.js
+++ src/selection/graphviz.js
@@ -1,11 +1,15 @@
 import {Graphviz} from "../graphviz";
+import {timeout} from "d3-timer";
 
 export default function(options) {
 
     var g = this.node().__graphviz__;
     if (g) {
         g.options(options);
-        g._dispatch.call("initEnd", this);
+        // Ensure a possible new initEnd event handler is attached before calling it
+        timeout(function () {
+            g._dispatch.call("initEnd", this);
+        }.bind(this), 0);
     } else {
         g = new Graphviz(this, options);
     }
