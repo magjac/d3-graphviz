19654ca68fe72df99cfc859addcbcb719c10b773
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-27 15:57:34 +0100

    Port data-test.js to hpcc-js-wasm

--- test/data-test.js
+++ test/data-test.js
@@ -6,24 +6,26 @@ var tape = require("tape"),
 tape("data() returns the data saved by dot().", function(test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph");
+    var graphviz = d3_graphviz.graphviz("#graph")
+        .on("initEnd", function () {
 
-    var savedData;
-    var attachedData;
+            var savedData;
+            var attachedData;
 
-    var noData = graphviz.data();
-    test.equal(noData, null, 'Data is not avaliable before calling dot()');
+            var noData = graphviz.data();
+            test.equal(noData, null, 'Data is not avaliable before calling dot()');
 
-    graphviz
-        .dot('digraph {a -> b;}', () => {
-            savedData = graphviz.data();
-            test.notEqual(savedData, null, 'Data is avaliable after calling dot()');
-        })
-       .render(() => {
-           savedData = graphviz.data();
-           test.notEqual(savedData, null, 'Data is still avaliable after calling render()');
-           test.equal(savedData, d3.select("#graph").selectAll("svg").datum(), 'The retrieved data is the same as the data attached to the #graph element');
+            graphviz
+                .dot('digraph {a -> b;}', () => {
+                    savedData = graphviz.data();
+                    test.notEqual(savedData, null, 'Data is avaliable after calling dot()');
+                })
+               .render(() => {
+                   savedData = graphviz.data();
+                   test.notEqual(savedData, null, 'Data is still avaliable after calling render()');
+                   test.equal(savedData, d3.select("#graph").selectAll("svg").datum(), 'The retrieved data is the same as the data attached to the #graph element');
 
-           test.end();
-       });
+                   test.end();
+               });
+        });
 });
