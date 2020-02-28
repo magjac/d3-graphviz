202ce2afb60019fc37ddd307643db237ac3cba51
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-28 15:03:35 +0100

    Port render-remove-attribute-test.js to hpcc-js-wasm

--- test/render-remove-attribute-test.js
+++ test/render-remove-attribute-test.js
@@ -9,16 +9,19 @@ tape("graphviz().render() removes attribute from SVG element when attribute is r
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
 
-    var graphviz = d3_graphviz.graphviz("#graph");
+    var graphviz = d3_graphviz.graphviz("#graph")
+        .on("initEnd", startTest);
 
-    graphviz
-        .zoom(false)
-        .renderDot('digraph {a [style=dashed]}', function() {
-            test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
-            graphviz
-                .renderDot('digraph {a}', function () {
-                    test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
-                    test.end();
-                });
-        });
+    function startTest() {
+        graphviz
+            .zoom(false)
+            .renderDot('digraph {a [style=dashed]}', function() {
+                test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
+                graphviz
+                    .renderDot('digraph {a}', function () {
+                        test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
+                        test.end();
+                    });
+            });
+    }
 });
