26a5de398d365a93da3e81272818bead62faf77d
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-28 13:36:32 +0100

    Adapt drawNode-shape-triangle-test.js data to Graphviz version 2.42.2

--- test/drawNode-shape-triangle-test.js
+++ test/drawNode-shape-triangle-test.js
@@ -4,7 +4,7 @@ var deepEqualData = require("./deepEqualData");
 var d3 = require("d3-selection");
 var d3_graphviz = require("../");
 var translatePointsAttribute = require("./svg").translatePointsAttribute;
-var roundTo4Decimals = require("./utils").roundTo4Decimals;
+var roundTo2Decimals = require("./utils").roundTo2Decimals;
 
 tape("Verify that triangle shape is drawn exactly as Graphviz does.", function(test) {
     var window = global.window = jsdom('<div id="expected-graph"></div><div id="actual-graph"></div>');
@@ -55,8 +55,8 @@ tape("Verify that triangle shape is drawn exactly as Graphviz does.", function(t
                     test.equal(translatePointsAttribute(actualNodeShape.attr("points"), -xoffs, -yoffs), expectedNodeShape.attr("points"), 'points of polygon');
 
                     test.equal(actualNodeText.attr("text-anchor"), expectedNodeText.attr("text-anchor"), 'text-anchor of text');
-                    test.equal(roundTo4Decimals(+actualNodeText.attr("x") - xoffs), +expectedNodeText.attr("x"), 'x of text');
-                    test.equal(roundTo4Decimals(+actualNodeText.attr("y") - yoffs), +expectedNodeText.attr("y"), 'y of text');
+                    test.equal(roundTo2Decimals(+actualNodeText.attr("x") - xoffs), +expectedNodeText.attr("x"), 'x of text');
+                      test.equal(roundTo2Decimals(+actualNodeText.attr("y") - yoffs), +expectedNodeText.attr("y"), 'y of text');
                     test.equal(actualNodeText.attr("font-family"), expectedNodeText.attr("font-family"), 'font-family of text');
                     test.equal(actualNodeText.attr("font-size"), expectedNodeText.attr("font-size"), 'font-size of text');
                     test.equal(actualNodeText.attr("fill"), expectedNodeText.attr("fill"), 'fill of text');
