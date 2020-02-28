11aed95dfea74262767b77981fe38bbdea9ff005
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-28 13:19:40 +0100

    Port drawNode-shape-cylinder-test.js to hpcc-js-wasm

--- test/drawNode-shape-cylinder-test.js
+++ test/drawNode-shape-cylinder-test.js
@@ -10,9 +10,14 @@ tape("Verify that cylinder shape is drawn exactly as Graphviz does.", function(t
     var document = global.document = window.document;
     var expectedGraph = d3.select("#expected-graph");
     var actualGraph = d3.select("#actual-graph");
-    var expectedGraphviz = d3_graphviz.graphviz("#expected-graph");
-    var actualGraphviz = d3_graphviz.graphviz("#actual-graph");
+    var actualGraphviz;
+    var expectedGraphviz = d3_graphviz.graphviz("#expected-graph")
+        .on('initEnd', () => {
+            actualGraphviz = d3_graphviz.graphviz("#actual-graph")
+                .on('initEnd', startTest);
+        });
 
+    function startTest() {
     expectedGraphviz
         .zoom(false)
         .renderDot('digraph {a [shape="cylinder"]}', function () {
@@ -68,4 +73,5 @@ tape("Verify that cylinder shape is drawn exactly as Graphviz does.", function(t
                     test.end();
                 });
         });
+    }
 });
