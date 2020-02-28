a500e1887e7fce92818439cff37ab3eadad78107
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-28 13:34:28 +0100

    Adapt drawNode-shape-egg-test.js data to Graphviz version 2.42.2

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,3 @@
-module.exports.roundTo4Decimals = function(x) {
-    return Math.round(x * 10000.0) / 10000.0
+module.exports.roundTo2Decimals = function(x) {
+    return Math.round(x * 100.0) / 100.0
 }
