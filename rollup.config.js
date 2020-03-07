860099547134d47e6c3096c268703a385883d37b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-03-07 16:22:05 +0100

    Upgrade build, test & coverage tools to latest versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default [
     {
-        entry: 'index.js',
-        dest: 'build/d3-graphviz.js',
-        format: 'umd',
-        moduleName: 'd3-graphviz',
-        sourceMap: (process.env.NODE_ENV != 'production'),
+        input: 'index.js',
+        output: {
+            file: 'build/d3-graphviz.js',
+            format: 'umd',
+            name: 'd3-graphviz',
+            sourcemap: true,
+            globals: {
+                'd3-selection': 'd3',
+                'd3-transition': 'd3',
+                'd3-interpolate': 'd3',
+                'd3-zoom': 'd3',
+                'd3-dispatch': 'd3',
+                'd3-format': 'd3',
+                'd3-timer': 'd3',
+                'd3-path': 'd3',
+                '@hpcc-js/wasm': '@hpcc-js/wasm',
+            },
+        },
         external: [
             'd3-selection',
             'd3-transition',
@@ -18,18 +32,8 @@ export default [
             'd3-path',
             '@hpcc-js/wasm',
         ],
-        globals: {
-            'd3-selection': 'd3',
-            'd3-transition': 'd3',
-            'd3-interpolate': 'd3',
-            'd3-zoom': 'd3',
-            'd3-dispatch': 'd3',
-            'd3-format': 'd3',
-            'd3-timer': 'd3',
-            'd3-path': 'd3',
-            '@hpcc-js/wasm': '@hpcc-js/wasm',
-        },
         plugins: [
+            resolve(),
             (process.env.NODE_ENV === 'production' && babel({
                 exclude: ['node_modules/**'],
             })),
