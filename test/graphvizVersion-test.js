e027a96942994ac57c0aede0641a88ea2a1b3acc
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-02-05 21:47:16 +0100

    add test of graphvizVersion() method

---
+++ test/graphvizVersion-test.js
@@ -0,0 +1,18 @@
+var tape = require("tape");
+var d3_graphviz = require("../");
+
+tape("graphviz().graphvizVersion() return the Graphviz version.", function(test) {
+
+    var graphviz = d3_graphviz.graphviz("#graph")
+        .on("initEnd", startTest);
+
+    function startTest() {
+        const version = graphviz.graphvizVersion();
+        const [major, minor, patch] = version.split('.');
+        test.ok(!isNaN(major), 'Major version number is a number');
+        test.ok(!isNaN(minor), 'Minor version number is a number');
+        test.ok(!isNaN(patch), 'Patch version number is a number');
+
+        test.end();
+    }
+});
