80e3f1d09f8e75334059063b6596658172f3010d
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-03 09:38:41 +0200

    port attributer-test.js to ESM modules

--- test/attributer-test.js
+++ test/attributer-test.js
@@ -1,11 +1,11 @@
-var tape = require("tape");
-var jsdom = require("./jsdom");
-var d3_graphviz = require("../");
+import assert from "assert";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
 
-tape("The attributer is called during rendering.", function(test) {
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
+const html = '<div id="graph"></div>';
+
+it("The attributer is called during rendering.", html, () => new Promise(resolve => {
+    var graphviz = d3_graphviz("#graph")
         .on("initEnd", function () {
 
             var tagCounts = [];
@@ -22,15 +22,15 @@ tape("The attributer is called during rendering.", function(test) {
         });
 
     function part2(tagCounts) {
-        test.equal(tagCounts['svg'], 1, "The attributer is called 1 time for 'svg' elements when enabled")
-        test.equal(tagCounts['g'], 4, "The attributer is called 4 times for 'g' elements when enabled")
-        test.equal(tagCounts['polygon'], 2, "The attributer is called 2 times for 'polygon' elements when enabled")
-        test.equal(tagCounts['ellipse'], 2, "The attributer is called 2 times for 'ellipse' elements when enabled")
-        test.equal(tagCounts['path'], 1, "The attributer is called 1 time for 'path' elements when enabled")
-        test.equal(tagCounts['title'], 3, "The attributer is called 3 times for 'title' elements when enabled")
-        test.equal(tagCounts['text'], 2, "The attributer is called 2 times for 'text' elements when enabled")
-        test.equal(tagCounts['#text'], 27, "The attributer is called 27 times for '#text' elements when enabled")
-        test.equal(tagCounts['#comment'], 3, "The attributer is called 3 times for '#text' elements when enabled")
+        assert.equal(tagCounts['svg'], 1, "The attributer is called 1 time for 'svg' elements when enabled")
+        assert.equal(tagCounts['g'], 4, "The attributer is called 4 times for 'g' elements when enabled")
+        assert.equal(tagCounts['polygon'], 2, "The attributer is called 2 times for 'polygon' elements when enabled")
+        assert.equal(tagCounts['ellipse'], 2, "The attributer is called 2 times for 'ellipse' elements when enabled")
+        assert.equal(tagCounts['path'], 1, "The attributer is called 1 time for 'path' elements when enabled")
+        assert.equal(tagCounts['title'], 3, "The attributer is called 3 times for 'title' elements when enabled")
+        assert.equal(tagCounts['text'], 2, "The attributer is called 2 times for 'text' elements when enabled")
+        assert.equal(tagCounts['#text'], 27, "The attributer is called 27 times for '#text' elements when enabled")
+        assert.equal(tagCounts['#comment'], 3, "The attributer is called 3 times for '#text' elements when enabled")
 
         for (let tag in tagCounts) {
             tagCounts[tag] = 0
@@ -44,15 +44,16 @@ tape("The attributer is called during rendering.", function(test) {
     }
 
     function part3(tagCounts) {
-        test.equal(tagCounts['svg'], 0, "The attributer is called 0 times for 'svg' elements when disabled")
-        test.equal(tagCounts['g'], 0, "The attributer is called 0 times for 'g' elements when disabled")
-        test.equal(tagCounts['polygon'], 0, "The attributer is called 0 times for 'polygon' elements when disabled")
-        test.equal(tagCounts['ellipse'], 0, "The attributer is called 0 times for 'ellipse' elements when disabled")
-        test.equal(tagCounts['path'], 0, "The attributer is called 0 times for 'path' elements when disabled")
-        test.equal(tagCounts['title'], 0, "The attributer is called 0 times for 'title' elements when disabled")
-        test.equal(tagCounts['text'], 0, "The attributer is called 0 times for 'text' elements when disabled")
-        test.equal(tagCounts['#text'], 0, "The attributer is called 0 times for '#text' elements when disabled")
-        test.equal(tagCounts['#comment'], 0, "The attributer is called 0 times for '#text' elements when disabled")
-        test.end();
+        assert.equal(tagCounts['svg'], 0, "The attributer is called 0 times for 'svg' elements when disabled")
+        assert.equal(tagCounts['g'], 0, "The attributer is called 0 times for 'g' elements when disabled")
+        assert.equal(tagCounts['polygon'], 0, "The attributer is called 0 times for 'polygon' elements when disabled")
+        assert.equal(tagCounts['ellipse'], 0, "The attributer is called 0 times for 'ellipse' elements when disabled")
+        assert.equal(tagCounts['path'], 0, "The attributer is called 0 times for 'path' elements when disabled")
+        assert.equal(tagCounts['title'], 0, "The attributer is called 0 times for 'title' elements when disabled")
+        assert.equal(tagCounts['text'], 0, "The attributer is called 0 times for 'text' elements when disabled")
+        assert.equal(tagCounts['#text'], 0, "The attributer is called 0 times for '#text' elements when disabled")
+        assert.equal(tagCounts['#comment'], 0, "The attributer is called 0 times for '#text' elements when disabled")
+
+        resolve();
     }
-});
+}));
