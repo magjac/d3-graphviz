5036e4f2382f669327f977704bbc2028b190049b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-03 11:51:29 +0200

    port destroy-shared-worker-test.js to ESM modules

--- test/@hpcc-js/wasm/dist/wrapper.js
+++ test/@hpcc-js/wasm/dist/wrapper.js
@@ -1,4 +1,3 @@
 importScripts('node_modules/@hpcc-js/wasm/dist/index.js');
 self["@hpcc-js/wasm"] = global["@hpcc-js/wasm"];
-importScripts('test/polyfill_fetch.js');
 global.document = {};
--- test/destroy-shared-worker-test.js
+++ test/destroy-shared-worker-test.js
@@ -1,44 +1,37 @@
-var tape = require("tape"),
-    jsdom = require("./jsdom"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
-const SharedWorker = require("./polyfill_SharedWorker");
+import assert from "assert";
+import {select as d3_select} from "d3-selection";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
+import SharedWorker from "./polyfill_SharedWorker.js";
 
-tape(".destroy() deletes the Graphviz instance from the container element (shared worker version)", function (test) {
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-    global.document = window.document;
+const html = `
+    <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+    <div id="graph"></div>
+    `;
+
+it(".destroy() deletes the Graphviz instance from the container element (shared worker version)", html, () => new Promise(resolve => {
     global.SharedWorker = SharedWorker;
 
-    var graphviz = d3_graphviz.graphviz("#graph", {useSharedWorker: true})
+    var graphviz = d3_graphviz("#graph", {useSharedWorker: true})
         .renderDot('digraph {a -> b;}', destroy);
 
     function destroy() {
 
-        test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
+        assert.notEqual(d3_select("#graph").node().__graphviz__, undefined,
                        'Renderer instance shall exist before destroy');
         graphviz.destroy();
-        test.equal(d3.select("#graph").node().__graphviz__, undefined,
+        assert.equal(d3_select("#graph").node().__graphviz__, undefined,
                        'Renderer instance shall not exist after destroy');
 
         graphviz._workerPortClose();
         global.SharedWorker = undefined;
-        test.end();
+        resolve();
     }
-});
 
-tape(".destroy() closes the shared worker", function(test) {
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-    var document = global.document = window.document;
+}));
+
+it(".destroy() closes the shared worker", html, () => new Promise(resolve => {
+
     var Blob = global.Blob = function (jsarray) {
         return new Function(jsarray[0]);
     }
@@ -47,7 +40,7 @@ tape(".destroy() closes the shared worker", function(test) {
     }
     global.SharedWorker = SharedWorker;
 
-    var graphviz = d3_graphviz.graphviz("#graph", {useSharedWorker: true})
+    var graphviz = d3_graphviz("#graph", {useSharedWorker: true})
         .renderDot('digraph {a -> b;}', destroy);
 
     function destroy() {
@@ -59,16 +52,17 @@ tape(".destroy() closes the shared worker", function(test) {
             setTimeout(() => {
                 graphviz._workerPort.onmessage = () => {
                     numberOfMessages += 1;
-                    test.fail('Worker shall not respond after close');
+                    assert.fail('Worker shall not respond after close');
                 }
                 graphviz._workerPort.postMessage({dot: '', engine: 'dot'});
-                test.equal(numberOfMessages, 1,
+                assert.equal(numberOfMessages, 1,
                            'One message shall have been received');
                 graphviz._workerPortClose();
                 global.SharedWorker = undefined;
-                test.end();
             }, 0);
         };
         graphviz._workerPort.postMessage({dot: '', engine: 'dot'});
+
+        resolve();
     }
-});
+}));
