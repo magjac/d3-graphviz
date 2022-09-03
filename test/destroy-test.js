6bb1055874f2667cd5014bbc736e97fde71b3689
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-03 12:36:16 +0200

    port destroy-test to ESM modules

--- test/destroy-test.js
+++ test/destroy-test.js
@@ -1,22 +1,22 @@
-var tape = require("tape"),
-    jsdom = require("./jsdom"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
+import assert from "assert";
+import {select as d3_select} from "d3-selection";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
 
-tape(".destroy() deletes the Graphviz instance from the container element", function(test) {
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph", {useWorker: false})
+const html = '<div id="graph"></div>';
+
+it(".destroy() deletes the Graphviz instance from the container element", html, () => new Promise(resolve => {
+    var graphviz = d3_graphviz("#graph", {useWorker: false})
         .renderDot('digraph {a -> b;}', destroy);
 
     function destroy() {
 
-        test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
+        assert.notEqual(d3_select("#graph").node().__graphviz__, undefined,
                        'Renderer instance shall exist before destroy');
         graphviz.destroy();
-        test.equal(d3.select("#graph").node().__graphviz__, undefined,
+        assert.equal(d3_select("#graph").node().__graphviz__, undefined,
                        'Renderer instance shall not exist after destroy');
 
-        test.end();
+        resolve();
     }
-});
+}));
