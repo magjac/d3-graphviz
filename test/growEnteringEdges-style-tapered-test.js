1eced3aa5ffb9d56416005bc4b761898a49f5efc
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-04 12:28:19 +0200

    port growEnteringEdges-style-tapered-test.js to ESM modules

--- test/growEnteringEdges-style-tapered-test.js
+++ test/growEnteringEdges-style-tapered-test.js
@@ -1,22 +1,22 @@
-var tape = require("tape"),
-    jsdom = require("./jsdom"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
+import assert from "assert";
+import {selectAll as d3_selectAll} from "d3-selection";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
 
-tape("Simple rendering an SVG from graphviz DOT.", function(test) {
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
+const html = '<div id="graph"></div>';
+
+it("Simple rendering an SVG from graphviz DOT.", html, () => new Promise(resolve => {
+    var graphviz = d3_graphviz("#graph")
         .growEnteringEdges(true)
         .renderDot('digraph {a -> b [style=tapered]}', checkGraph);
 
     function checkGraph() {
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons');
-        test.equal(d3.selectAll('path').size(), 0, 'Number of paths');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges');
+        assert.equal(d3_selectAll('ellipse').size(), 2, 'Number of ellipses');
+        assert.equal(d3_selectAll('polygon').size(), 3, 'Number of polygons');
+        assert.equal(d3_selectAll('path').size(), 0, 'Number of paths');
 
-        test.end();
+        resolve();
     }
-});
+}));
