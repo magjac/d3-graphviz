854a6e4a4fe068406cfbe17b36e37d22f5021f79
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-04 13:41:43 +0200

    port render-remove-attribute-test.js to ESM modules

--- test/render-remove-attribute-test.js
+++ test/render-remove-attribute-test.js
@@ -1,27 +1,27 @@
-var tape = require("tape");
-var jsdom = require("./jsdom");
-var d3 = require("d3-selection");
-var d3_transition = require("d3-transition");
-var d3_graphviz = require("../");
+import assert from "assert";
+import {selectAll as d3_selectAll} from "d3-selection";
+import {transition as d3_transition} from "d3-transition";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
 
-tape("graphviz().render() removes attribute from SVG element when attribute is removed from Graphviz node.", function(test) {
+const html = '<div id="graph"></div>';
 
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
+it("graphviz().render() removes attribute from SVG element when attribute is removed from Graphviz node.", html, () => new Promise(resolve => {
 
-    var graphviz = d3_graphviz.graphviz("#graph")
+
+    var graphviz = d3_graphviz("#graph")
         .on("initEnd", startTest);
 
     function startTest() {
         graphviz
             .zoom(false)
             .renderDot('digraph {a [style=dashed]}', function() {
-                test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
+                assert.equal(d3_selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
                 graphviz
                     .renderDot('digraph {a}', function () {
-                        test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
-                        test.end();
+                        assert.equal(d3_selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
+                        resolve();
                     });
             });
     }
-});
+}));
