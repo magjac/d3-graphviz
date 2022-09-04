74ee32d08095d74fec03af194f19edb49b17f79a
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-04 23:07:40 +0200

    port tweenShapes-test.js to ESM modules

--- test/tweenShapes-test.js
+++ test/tweenShapes-test.js
@@ -1,14 +1,14 @@
-var tape = require("tape");
-var jsdom = require("./jsdom");
-var d3 = require("d3-selection");
-var d3_transition = require("d3-transition");
-var d3_graphviz = require("../");
+import assert from "assert";
+import {selectAll as d3_selectAll} from "d3-selection";
+import {transition as d3_transition} from "d3-transition";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
 
-tape("graphviz().tweenShapes() enables and disables shape tweening during transitions.", function(test) {
+const html = '<div id="graph"></div>';
 
-    var window = global.window = jsdom('<div id="graph"></div>');
-    var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
+it("graphviz().tweenShapes() enables and disables shape tweening during transitions.", html, () => new Promise(resolve => {
+
+    var graphviz = d3_graphviz("#graph")
         .on('initEnd', startTest)
 
     function startTest() {
@@ -19,55 +19,55 @@ tape("graphviz().tweenShapes() enables and disables shape tweening during transi
             .convertEqualSidedPolygons(false)
             .dot('digraph {a -> b;}')
             .render();
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of initial nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of initial ellipses');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
-        const transition1 = d3_transition.transition().duration(0);
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of initial nodes');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of initial edges');
+        assert.equal(d3_selectAll('ellipse').size(), 2, 'Number of initial ellipses');
+        assert.equal(d3_selectAll('polygon').size(), 2, 'Number of initial polygons');
+        assert.equal(d3_selectAll('path').size(), 1, 'Number of initial paths');
+        const transition1 = d3_transition().duration(0);
         graphviz
             .dot('digraph {a [shape="box"];a -> b}')
             .transition(transition1)
             .render()
             .on ("end", part1_end);
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes immediately after rendering');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
-        test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses immediately after rendering');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons immediately after rendering');
-        test.equal(d3.selectAll('path').size(), 2, 'Number of paths immediately after rendering');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes immediately after rendering');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
+        assert.equal(d3_selectAll('ellipse').size(), 1, 'Number of ellipses immediately after rendering');
+        assert.equal(d3_selectAll('polygon').size(), 2, 'Number of polygons immediately after rendering');
+        assert.equal(d3_selectAll('path').size(), 2, 'Number of paths immediately after rendering');
 
     }
 
     function part1_end() {
 
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after shape change');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges after shape change');
-        test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses after shape change');
-        test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after shape change');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths after shape change');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes after shape change');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges after shape change');
+        assert.equal(d3_selectAll('ellipse').size(), 1, 'Number of ellipses after shape change');
+        assert.equal(d3_selectAll('polygon').size(), 3, 'Number of polygons after shape change');
+        assert.equal(d3_selectAll('path').size(), 1, 'Number of paths after shape change');
 
-        const transition1 = d3_transition.transition().duration(0);
+        const transition1 = d3_transition().duration(0);
         graphviz
             .dot('digraph {node [shape="triangle"];a -> b}')
             .transition(transition1)
             .render()
             .on ("end", part2_end);
 
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes immediately after rendering');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
-        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses immediately after rendering');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons immediately after rendering');
-        test.equal(d3.selectAll('path').size(), 3, 'Number of paths immediately after rendering');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes immediately after rendering');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
+        assert.equal(d3_selectAll('ellipse').size(), 0, 'Number of ellipses immediately after rendering');
+        assert.equal(d3_selectAll('polygon').size(), 2, 'Number of polygons immediately after rendering');
+        assert.equal(d3_selectAll('path').size(), 3, 'Number of paths immediately after rendering');
     }
 
     function part2_end() {
 
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after shape change');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges after shape change');
-        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after shape change');
-        test.equal(d3.selectAll('polygon').size(), 4, 'Number of polygons after shape change');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths after shape change');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes after shape change');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges after shape change');
+        assert.equal(d3_selectAll('ellipse').size(), 0, 'Number of ellipses after shape change');
+        assert.equal(d3_selectAll('polygon').size(), 4, 'Number of polygons after shape change');
+        assert.equal(d3_selectAll('path').size(), 1, 'Number of paths after shape change');
 
-        test.end();
+        resolve();
     }
-});
+}));
