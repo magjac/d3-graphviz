730b8002f079a994fa6680877887089c0232cc05
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-05 14:16:32 +0200

    port transition-test.js to ESM modules

--- test/transition-test.js
+++ test/transition-test.js
@@ -1,24 +1,22 @@
-var tape = require("tape");
-var jsdom = require("./jsdom");
-var d3 = require("d3-selection");
-var d3_transition = require("d3-transition");
-var d3_graphviz = require("../");
-var Worker = require("tiny-worker");
-var hpccWasm = require("@hpcc-js/wasm");
+import assert from "assert";
+import {selectAll as d3_selectAll} from "d3-selection";
+import {transition as d3_transition} from "d3-transition";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
+import Worker from "tiny-worker";
+import hpccWasm from "@hpcc-js/wasm";
 
-tape("graphviz().render() adds and removes SVG elements after transition delay.", function(test) {
+const html = `
+    <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+    <div id="graph"></div>
+    `;
+
+it("graphviz().render() adds and removes SVG elements after transition delay.", html, () => new Promise(resolve => {
 
     var savedGraphviz = hpccWasm.graphviz
     delete hpccWasm.graphviz;
 
     function transition_test_init() {
-        var window = global.window = jsdom(
-            `
-                <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-                <div id="graph"></div>
-                `,
-        );
-        var document = global.document = window.document;
         var Blob = global.Blob = function (jsarray) {
             return new Function(jsarray[0]);
         }
@@ -30,16 +28,16 @@ tape("graphviz().render() adds and removes SVG elements after transition delay."
 
     function transition_test(transition1, next_test) {
 
-        var graphviz = d3_graphviz.graphviz("#graph");
+        var graphviz = d3_graphviz("#graph");
 
-        test.equal(graphviz.active(), null, 'No transition is active before a graph has been rendered');
+        assert.equal(graphviz.active(), null, 'No transition is active before a graph has been rendered');
 
         graphviz
             .on("initEnd", function () {
                 part1();
             });
 
-        const transition2 = transition1 || d3_transition.transition().duration(1000);
+        const transition2 = transition1 || d3_transition().duration(1000);
         function part1() {
             graphviz
                 .tweenShapes(false)
@@ -48,22 +46,22 @@ tape("graphviz().render() adds and removes SVG elements after transition delay."
                 .dot('digraph {a -> b; c}')
                 .render(part1_end)
                 .on("transitionStart", function () {
-                    test.equal(graphviz.active(), null, 'No transition is active before the transition starts');
+                    assert.equal(graphviz.active(), null, 'No transition is active before the transition starts');
                 })
                 .on("transitionEnd", function () {
-                    test.ok(graphviz.active() instanceof d3_transition.transition, 'A transition is active just before the transition ends');
+                    assert.ok(graphviz.active() instanceof d3_transition, 'A transition is active just before the transition ends');
                 });
         }
 
         function part1_end() {
-            test.equal(graphviz.active(), null, 'No transition is active after the transition ended');
-            test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-            test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
+            assert.equal(graphviz.active(), null, 'No transition is active after the transition ended');
+            assert.equal(d3_selectAll('.node').size(), 3, 'Number of initial nodes');
+            assert.equal(d3_selectAll('.edge').size(), 1, 'Number of initial edges');
+            assert.equal(d3_selectAll('polygon').size(), 2, 'Number of initial polygons');
+            assert.equal(d3_selectAll('ellipse').size(), 3, 'Number of initial ellipses');
+            assert.equal(d3_selectAll('path').size(), 1, 'Number of initial paths');
 
-            const transition2 = transition1 || d3_transition.transition().duration(1000);
+            const transition2 = transition1 || d3_transition().duration(1000);
             graphviz
                 .dot('digraph {a -> b; b -> a}')
                 .transition(transition1)
@@ -75,31 +73,31 @@ tape("graphviz().render() adds and removes SVG elements after transition delay."
         }
 
         function part2_end() {
-            test.equal(graphviz.active(), null, 'No transition is active immediately after the rendering has been initiated');
-            test.equal(d3.selectAll('.node').size(), 3, 'Number of nodes immediately after rendering');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
-            test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons immediately after rendering');
-            test.equal(d3.selectAll('ellipse').size(), 3, 'Number of ellipses immediately after rendering');
-            test.equal(d3.selectAll('path').size(), 2, 'Number of paths immediately after rendering');
+            assert.equal(graphviz.active(), null, 'No transition is active immediately after the rendering has been initiated');
+            assert.equal(d3_selectAll('.node').size(), 3, 'Number of nodes immediately after rendering');
+            assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
+            assert.equal(d3_selectAll('polygon').size(), 3, 'Number of polygons immediately after rendering');
+            assert.equal(d3_selectAll('ellipse').size(), 3, 'Number of ellipses immediately after rendering');
+            assert.equal(d3_selectAll('path').size(), 2, 'Number of paths immediately after rendering');
         }
 
         function part3_end() {
 
-            test.equal(graphviz.active(), null, 'No transition is active after the transition ended');
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after transition');
-            test.equal(d3.selectAll('.edge').size(), 2, 'Number of edges after transition');
-            test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after transition');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses after transition');
-            test.equal(d3.selectAll('path').size(), 2, 'Number of paths after transition');
+            assert.equal(graphviz.active(), null, 'No transition is active after the transition ended');
+            assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes after transition');
+            assert.equal(d3_selectAll('.edge').size(), 2, 'Number of edges after transition');
+            assert.equal(d3_selectAll('polygon').size(), 3, 'Number of polygons after transition');
+            assert.equal(d3_selectAll('ellipse').size(), 2, 'Number of ellipses after transition');
+            assert.equal(d3_selectAll('path').size(), 2, 'Number of paths after transition');
 
             if (next_test) {
-                graphviz._worker.terminate();
+                graphviz.destroy();
                 next_test();
             } else {
                 graphviz._worker.terminate();
                 global.Worker = undefined;
                 hpccWasm.graphviz = savedGraphviz;
-                test.end();
+                resolve();
             }
         }
     }
@@ -112,9 +110,9 @@ tape("graphviz().render() adds and removes SVG elements after transition delay."
     function transition_function_test() {
         transition_test_init();
         transition_test(function() {
-            return d3_transition.transition().duration(0);
+            return d3_transition().duration(0);
         });
     }
 
     transition_instance_test();
-});
+}));
