ec2e5fd97ecde9752513880b7c57857dbe738fc2
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-07 12:55:40 +0200

    add test/simple-worker-local-url-test.js

--- test/simple-worker-local-url-test.js
+++ test/simple-worker-local-url-test.js
@@ -1,20 +1,20 @@
-var tape = require("tape"),
-    jsdom = require("./jsdom"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
-var Worker = require("tiny-worker");
+import assert from "assert";
+import {selectAll as d3_selectAll} from "d3-selection";
+import {graphviz as d3_graphviz} from "../index.js";
+import it from "./jsdom.js";
+import Worker from "tiny-worker";
 
-tape("Simple rendering an SVG from graphviz DOT.", function(test) {
-    var window = global.window = jsdom(
-        `
-            <script src="test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-        {
-            url: "http:dummyhost",
-        },
-    );
-    var document = global.document = window.document;
+const html = `
+    <script src="test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+    <div id="graph"></div>
+    `;
+
+const options = {
+    url: "http:dummyhost",
+};
+
+it("Simple rendering an SVG from graphviz DOT using worker with local @hpcc-js/wasm script", html, options, () => new Promise(resolve => {
+
     var Blob = global.Blob = function (jsarray) {
         return new Function(jsarray[0]);
     }
@@ -23,18 +23,19 @@ tape("Simple rendering an SVG from graphviz DOT.", function(test) {
     }
     global.Worker = Worker;
 
-    var graphviz = d3_graphviz.graphviz("#graph")
+    var graphviz = d3_graphviz("#graph")
         .renderDot('digraph {a -> b;}', checkGraph);
 
     function checkGraph() {
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+        assert.equal(d3_selectAll('.node').size(), 2, 'Number of nodes');
+        assert.equal(d3_selectAll('.edge').size(), 1, 'Number of edges');
+        assert.equal(d3_selectAll('ellipse').size(), 2, 'Number of ellipses');
+        assert.equal(d3_selectAll('polygon').size(), 2, 'Number of polygons');
+        assert.equal(d3_selectAll('path').size(), 1, 'Number of paths');
 
         graphviz._worker.terminate();
         global.Worker = undefined;
-        test.end();
+
+        resolve();
     }
-});
+}));
