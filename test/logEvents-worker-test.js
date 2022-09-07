667f171e05915504fe8afb139ca16ebb5c79c243
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-09-07 13:36:00 +0200

    factor out logEvent test in web worker to new test logEvents-worker-test.js
    We need to test logEvents also in a web worker in order to get
    coverage for the 'initEnd' event which is registered too late if not
    using a web worker. It's better to have a separate test for this than
    to have logEvents enabled in various other tests.

---
+++ test/logEvents-worker-test.js
@@ -0,0 +1,47 @@
+var tape = require("tape");
+var jsdom = require("./jsdom");
+var d3_graphviz = require("../");
+var d3_transition = require("d3-transition");
+var Worker = require("tiny-worker");
+
+tape("logEvents enables event logging using web worker.", function(test) {
+    var window = global.window = jsdom(
+        `
+            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+            <div id="graph"></div>
+            `,
+    );
+    global.document = window.document;
+    global.Blob = function (jsarray) {
+        return new Function(jsarray[0]);
+    }
+    window.URL.createObjectURL = function (js) {
+        return js;
+    }
+    global.Worker = Worker;
+
+    var graphviz = d3_graphviz.graphviz("#graph")
+        .zoom(false)
+        .logEvents(true)
+        .renderDot('digraph {a -> b}')
+        .on("initEnd", checkEventLogging);
+
+    function checkEventLogging() {
+        var eventTypes = graphviz._eventTypes;
+        var n = 0;
+        for (let i in eventTypes) {
+            let eventType = eventTypes[i];
+            test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
+            n += 1;
+        }
+        test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
+        test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
+        graphviz.on("end", () => {
+            graphviz._worker.terminate();
+            global.Worker = undefined;
+            test.end();
+        });
+        graphviz._worker.terminate();
+        global.Worker = undefined;
+    }
+});
