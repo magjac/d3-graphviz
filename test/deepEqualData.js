2d14945d8d32a9a92e680f7ee24f4390fc5f473b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-24 12:34:11 +0100

    add ES module deepEqualData.js

---
+++ test/deepEqualData.js
@@ -0,0 +1,32 @@
+import assert from "assert";
+import deepEqual from 'deep-equal';
+
+export default function (test, actualData, expectedData, message) {
+
+    function parseData(parent) {
+
+        for (let i in parent.children) {
+            const child = parent.children[i]
+            if (typeof child.parent != 'string') {
+                assert.equal(parent, child.parent,
+                           'Parent of "' + child.tag + '" "' + child.id + '" is "' +
+                           parent.tag + '" "' + parent.id + '"');
+            }
+            delete child.parent
+            parseData(child)
+        }
+    }
+
+    parseData(expectedData);
+    parseData(actualData);
+
+    if (!deepEqual(actualData, expectedData)) {
+        console.log('actualData:');
+        console.log(JSON.stringify(actualData, null, 4));
+        console.log('expectedData:');
+        console.log(JSON.stringify(expectedData, null, 4));
+    }
+
+    assert.deepEqual(expectedData, actualData, message);
+
+}
