cf1e0314e80cf3f8ac2c92abc7d037f28328cb9d
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-24 12:46:15 +0100

    port test/drawNode-shape-rect-test.js to ESM

--- test/drawNode-shape-rect-test.js
+++ test/drawNode-shape-rect-test.js
@@ -1,9 +1,9 @@
-var tape = require("./tape.cjs");
-var jsdom = require("./jsdom.cjs");
-var deepEqualData = require("./deepEqualData.cjs");
-var d3 = require("d3-selection");
-var d3_graphviz = require("../");
-var translatePointsAttribute = require("./svg.cjs").translatePointsAttribute;
+import tape from "./tape.js";
+import jsdom from "./jsdom.js";
+import deepEqualData from "./deepEqualData.js";
+import * as d3 from "d3-selection";
+import * as d3_graphviz from "../index.js";
+import {translatePointsAttribute} from "../src/svg.js"
 
 tape("Verify that rect shape is drawn exactly as Graphviz does.", function(test) {
     var window = global.window = jsdom('<div id="expected-graph"></div><div id="actual-graph"></div>');
