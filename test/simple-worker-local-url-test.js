295fe50a1fcf81cdc1b2b9efcc7735f2bb952988
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-24 16:44:37 +0100

    port test/simple-worker-local-url-test.js to ESM

--- test/simple-worker-local-url-test.js
+++ test/simple-worker-local-url-test.js
@@ -1,8 +1,8 @@
-var tape = require("./tape.cjs"),
-    jsdom = require("./jsdom.cjs"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
-var Worker = require("tiny-worker");
+import tape from "./tape.js";
+import jsdom from "./jsdom.js";
+import * as d3 from "d3-selection";
+import * as d3_graphviz from "../index.js";
+import Worker from "tiny-worker";
 
 tape("Simple rendering an SVG from graphviz DOT.", function(test) {
     var window = global.window = jsdom(
