1924a0d89e7ce7c28c379f98bfeb1dc400594011
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-24 12:16:31 +0100

    port test/destroy-worker-test.js to ESM

--- test/destroy-worker-test.js
+++ test/destroy-worker-test.js
@@ -1,8 +1,8 @@
-var tape = require("./tape.cjs"),
-    jsdom = require("./jsdom.cjs"),
-    d3 = require("d3-selection"),
-    d3_graphviz = require("../");
-const Worker = require("tiny-worker");
+import tape from "./tape.js";
+import jsdom from "./jsdom.js";
+import * as d3 from "d3-selection";
+import * as d3_graphviz from "../index.js";
+import Worker from "tiny-worker";
 
 tape(".destroy() deletes the Graphviz instance from the container element (worker version)", function (test) {
     var window = global.window = jsdom(
