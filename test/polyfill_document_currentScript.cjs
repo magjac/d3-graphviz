47b973551fd2dc8dc78885551b933e3be2e3ec67
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-25 11:26:00 +0100

    remove now unsed test/polyfill_document_currentScript.cjs

--- test/polyfill_document_currentScript.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = function(document) {
-    Object.defineProperty(document, "currentScript", {
-        get() { return {src: './node_modules/@hpcc-js/wasm/dist/index.js'}; }
-    });
-}
