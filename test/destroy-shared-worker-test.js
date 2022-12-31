219575896e8215133e0b3ff65829750ccfa55315
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-31 13:43:39 +0100

    refactor test/destroy-shared-worker-test.js to use await promise

--- test/destroy-shared-worker-test.js
+++ test/destroy-shared-worker-test.js
@@ -4,7 +4,7 @@ import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import SharedWorker from "./polyfill_SharedWorker.js";
 
-tape(".destroy() deletes the Graphviz instance from the container element (shared worker version)", function (test) {
+tape(".destroy() deletes the Graphviz instance from the container element (shared worker version)", async function (test) {
     var window = global.window = jsdom(
         `
             <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
@@ -14,44 +14,44 @@ tape(".destroy() deletes the Graphviz instance from the container element (share
     global.document = window.document;
     global.SharedWorker = SharedWorker;
 
-    var graphviz = d3_graphviz.graphviz("#graph", {useSharedWorker: true})
-        .renderDot('digraph {a -> b;}', destroy);
+    var graphviz = d3_graphviz.graphviz("#graph", { useSharedWorker: true });
 
-    function destroy() {
+    await new Promise(resolve => {
+        graphviz
+            .renderDot('digraph {a -> b;}', resolve);
+    });
 
-        test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
-                       'Renderer instance shall exist before destroy');
-        graphviz.destroy();
-        test.equal(d3.select("#graph").node().__graphviz__, undefined,
-                       'Renderer instance shall not exist after destroy');
 
-        graphviz._workerPortClose();
-        global.SharedWorker = undefined;
-        test.end();
-    }
+    test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
+        'Renderer instance shall exist before destroy');
+    graphviz.destroy();
+    test.equal(d3.select("#graph").node().__graphviz__, undefined,
+        'Renderer instance shall not exist after destroy');
+
+    graphviz._workerPortClose();
+    global.SharedWorker = undefined;
+    test.end();
+
 });
 
-tape(".destroy() closes the shared worker", function(test) {
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-    var document = global.document = window.document;
-    var Blob = global.Blob = function (jsarray) {
+tape(".destroy() closes the shared worker", async (test) => {
+
+    global.Blob = function (jsarray) {
         return new Function(jsarray[0]);
     }
-    var createObjectURL = window.URL.createObjectURL = function (js) {
+    window.URL.createObjectURL = function (js) {
         return js;
     }
     global.SharedWorker = SharedWorker;
 
-    var graphviz = d3_graphviz.graphviz("#graph", {useSharedWorker: true})
-        .renderDot('digraph {a -> b;}', destroy);
+    var graphviz = d3_graphviz.graphviz("#graph", { useSharedWorker: true });
 
-    function destroy() {
+    await new Promise(resolve => {
+        graphviz
+            .renderDot('digraph {a -> b;}', resolve);
+    });
 
+    await new Promise(resolve => {
         let numberOfMessages = 0;
         graphviz._workerPort.onmessage = () => {
             numberOfMessages += 1;
@@ -66,9 +66,11 @@ tape(".destroy() closes the shared worker", function(test) {
                            'One message shall have been received');
                 graphviz._workerPortClose();
                 global.SharedWorker = undefined;
-                test.end();
+                resolve();
             }, 0);
         };
         graphviz._workerPort.postMessage({dot: '', engine: 'dot'});
-    }
+    });
+
+    test.end();
 });
