89655871663c1989498a1d3de4f7287f9ed131a5
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-31 14:08:20 +0100

    refactor test/dot-empty-node-id-test.js to use await promise

--- test/dot-empty-node-id-test.js
+++ test/dot-empty-node-id-test.js
@@ -3,21 +3,27 @@ import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape("renderDot() renders a node with an empty string as node_id.", function(test) {
+tape("renderDot() renders a node with an empty string as node_id.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
 
-            graphviz
-                .tweenShapes(false)
-                .zoom(false)
-                .dot('digraph {""}')
-                .render();
+    var graphviz = d3_graphviz.graphviz("#graph");
 
-            test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges');
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    })
 
-            test.end();
-        });
+    await new Promise(resolve => {
+        graphviz
+            .tweenShapes(false)
+            .zoom(false)
+            .dot('digraph {""}')
+            .render(resolve);
+    });
+
+    test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges');
+
+    test.end();
 });
