fec4adc29200fa72dbae3255ac3374703d642626
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-31 14:29:17 +0100

    refactor test/dot-viewport-test.js to use await promise

--- test/dot-viewport-test.js
+++ test/dot-viewport-test.js
@@ -9,46 +9,50 @@ var margin = 4;
 var bbWidth = simpleWidth - margin * 2;
 var bbHeight = simpleHeight - margin * 2;
 
-tape("DOT without viewport", function(test) {
+tape("DOT without viewport", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    });
 
-            var scale = 1;
+    var scale = 1;
 
-            var dot = `
+    var dot = `
                 digraph {
                     graph[viewport="${simpleWidth},${simpleHeight},${scale}"]
                     a -> b
                 }
             `;
 
+    graphviz
+        .renderDot(dot);
 
-            graphviz
-                .renderDot(dot);
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
 
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(1)', 'transform attribute');
 
-            test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(1)', 'transform attribute');
-
-            test.end();
-        });
+    test.end();
 });
 
-tape("DOT with viewport scale 2", function(test) {
+tape("DOT with viewport scale 2", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    });
 
-            var scale = 2;
+    var scale = 2;
 
-            var dot = `
+    var dot = `
                 digraph {
                     graph[viewport="${simpleWidth * scale},${simpleHeight * scale},${scale}"]
                     a -> b
@@ -56,30 +60,32 @@ tape("DOT with viewport scale 2", function(test) {
             `;
 
 
-            graphviz
-                .renderDot(dot);
+    graphviz
+        .renderDot(dot);
 
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
 
-            test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(2)', 'transform attribute');
+    test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(2)', 'transform attribute');
 
-            test.end();
-        });
+
+    test.end();
 });
 
-tape("DOT with viewport scale 0.5", function(test) {
+tape("DOT with viewport scale 0.5", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    });
+    var scale = 0.5;
 
-            var scale = 0.5;
-
-            var dot = `
+    var dot = `
                 digraph {
                     graph[viewport="${simpleWidth * scale},${simpleHeight * scale},${scale}"]
                     a -> b
@@ -87,30 +93,33 @@ tape("DOT with viewport scale 0.5", function(test) {
             `;
 
 
-            graphviz
-                .renderDot(dot);
+    graphviz
+        .renderDot(dot);
 
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
 
-            test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(0.5)', 'transform attribute');
+    test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(0.5)', 'transform attribute');
 
-            test.end();
-        });
+
+    test.end();
 });
 
-tape("DOT with viewport scale 1.5", function(test) {
+tape("DOT with viewport scale 1.5", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    });
 
-        var scale = 1.5;
+    var scale = 1.5;
 
-        var dot = `
+    var dot = `
             digraph {
                 graph[viewport="${simpleWidth * scale},${simpleHeight * scale},${scale}"]
                 a -> b
@@ -118,30 +127,33 @@ tape("DOT with viewport scale 1.5", function(test) {
         `;
 
 
-        graphviz
-            .renderDot(dot);
+    graphviz
+        .renderDot(dot);
 
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
 
-        test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(1.5)', 'transform attribute');
+    test.equal(d3.select('g').attr("transform"), 'translate(4,112) scale(1.5)', 'transform attribute');
 
-        test.end();
-        });
+
+    test.end();
 });
 
-tape("DOT with viewport scale 2 and original size", function(test) {
+tape("DOT with viewport scale 2 and original size", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', () => {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on('initEnd', resolve);
+    });
 
-            var scale = 2;
+    var scale = 2;
 
-            var dot = `
+    var dot = `
                 digraph {
                     graph[viewport="${simpleWidth},${simpleHeight},${scale}"]
                     a -> b
@@ -149,17 +161,16 @@ tape("DOT with viewport scale 2 and original size", function(test) {
             `;
 
 
-            graphviz
-                .renderDot(dot);
+    graphviz
+        .renderDot(dot);
 
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
 
-            test.equal(d3.select('g').attr("transform"), 'translate(-11.5,83) scale(2)', 'transform attribute');
+    test.equal(d3.select('g').attr("transform"), 'translate(-11.5,83) scale(2)', 'transform attribute');
 
-            test.end();
-        });
+    test.end();
 });
