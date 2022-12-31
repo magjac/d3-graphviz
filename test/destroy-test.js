46fcb2502721085242d89852e217ca49186183c0
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-12-31 13:49:50 +0100

    refactor test/destroy-test.js to use await promise

--- test/destroy-test.js
+++ test/destroy-test.js
@@ -3,20 +3,21 @@ import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape(".destroy() deletes the Graphviz instance from the container element", function(test) {
+tape(".destroy() deletes the Graphviz instance from the container element", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph", {useWorker: false})
-        .renderDot('digraph {a -> b;}', destroy);
+    var graphviz = d3_graphviz.graphviz("#graph", { useWorker: false });
 
-    function destroy() {
+    await new Promise(resolve => {
+        graphviz
+            .renderDot('digraph {a -> b;}', resolve);
+    });
 
-        test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
-                       'Renderer instance shall exist before destroy');
-        graphviz.destroy();
-        test.equal(d3.select("#graph").node().__graphviz__, undefined,
-                       'Renderer instance shall not exist after destroy');
+    test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
+        'Renderer instance shall exist before destroy');
+    graphviz.destroy();
+    test.equal(d3.select("#graph").node().__graphviz__, undefined,
+        'Renderer instance shall not exist after destroy');
 
-        test.end();
-    }
+    test.end()
 });
