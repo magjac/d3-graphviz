6d48d43e3014b630fb859c8cfea88f0443c4bab8
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 13:16:38 +0100

    refactor test/destroy-shared-worker-test.js to directly use assert instead of tape shim

--- test/destroy-shared-worker-test.js
+++ test/destroy-shared-worker-test.js
@@ -1,10 +1,11 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import SharedWorker from "./polyfill_SharedWorker.js";
 
-tape(".destroy() deletes the Graphviz instance from the container element (shared worker version)", async function (test) {
+it(".destroy() deletes the Graphviz instance from the container element (shared worker version)", async () => {
     var window = global.window = jsdom(
         `
             <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
@@ -22,19 +23,17 @@ tape(".destroy() deletes the Graphviz instance from the container element (share
     });
 
 
-    test.notEqual(d3.select("#graph").node().__graphviz__, undefined,
+    assert.notEqual(d3.select("#graph").node().__graphviz__, undefined,
         'Renderer instance shall exist before destroy');
     graphviz.destroy();
-    test.equal(d3.select("#graph").node().__graphviz__, undefined,
+    assert.equal(d3.select("#graph").node().__graphviz__, undefined,
         'Renderer instance shall not exist after destroy');
 
     graphviz._workerPortClose();
     global.SharedWorker = undefined;
-    test.end();
-
 });
 
-tape(".destroy() closes the shared worker", async (test) => {
+it(".destroy() closes the shared worker", async (test) => {
 
     global.Blob = function (jsarray) {
         return new Function(jsarray[0]);
@@ -59,10 +58,10 @@ tape(".destroy() closes the shared worker", async (test) => {
             setTimeout(() => {
                 graphviz._workerPort.onmessage = () => {
                     numberOfMessages += 1;
-                    test.fail('Worker shall not respond after close');
+                    assert.fail('Worker shall not respond after close');
                 }
                 graphviz._workerPort.postMessage({dot: '', engine: 'dot'});
-                test.equal(numberOfMessages, 1,
+                assert.equal(numberOfMessages, 1,
                            'One message shall have been received');
                 graphviz._workerPortClose();
                 global.SharedWorker = undefined;
@@ -71,6 +70,4 @@ tape(".destroy() closes the shared worker", async (test) => {
         };
         graphviz._workerPort.postMessage({dot: '', engine: 'dot'});
     });
-
-    test.end();
 });
