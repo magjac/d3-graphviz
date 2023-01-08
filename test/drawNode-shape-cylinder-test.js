1c009031fb33e74dbd55402efa01d84ae4184f9c
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 16:55:24 +0100

    refactor test/drawNode-shape-cylinder-test.js to directly use assert instead of tape shim

--- test/drawNode-shape-cylinder-test.js
+++ test/drawNode-shape-cylinder-test.js
@@ -1,11 +1,12 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import deepEqualData from "./deepEqualData.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import {translateDAttribute} from "../src/svg.js"
 
-tape("Verify that cylinder shape is drawn exactly as Graphviz does.", async function (test) {
+it("Verify that cylinder shape is drawn exactly as Graphviz does.", async () => {
     var window = global.window = jsdom('<div id="expected-graph"></div><div id="actual-graph"></div>');
     var document = global.document = window.document;
     var expectedGraph = d3.select("#expected-graph");
@@ -55,30 +56,29 @@ tape("Verify that cylinder shape is drawn exactly as Graphviz does.", async func
     var xoffs = x - bbox.cx;
     var yoffs = y - bbox.cy;
 
-    test.equal(actualNodeGroup.attr("id"), expectedNodeGroup.attr("id"), 'id of group');
+    assert.equal(actualNodeGroup.attr("id"), expectedNodeGroup.attr("id"), 'id of group');
 
-    test.equal(actualNodeTitle.text(), expectedNodeTitle.text(), 'text of title');
+    assert.equal(actualNodeTitle.text(), expectedNodeTitle.text(), 'text of title');
 
     actualNodeShapes.each(function (d, i, nodes) {
         var actualNodeShape = d3.select(this);
         var expectedNodeShape = d3.select(expectedNodeShapes.nodes()[i]);
-        test.equal(actualNodeShape.attr("fill"), expectedNodeShape.attr("fill"), 'fill of path ' + (i + 1));
-        test.equal(actualNodeShape.attr("stroke"), expectedNodeShape.attr("stroke"), 'stroke of path ' + (i + 1));
-        test.equal(actualNodeShape.attr("d"), translateDAttribute(expectedNodeShape.attr("d"), xoffs, yoffs), 'd of path ' + (i + 1));
+        assert.equal(actualNodeShape.attr("fill"), expectedNodeShape.attr("fill"), 'fill of path ' + (i + 1));
+        assert.equal(actualNodeShape.attr("stroke"), expectedNodeShape.attr("stroke"), 'stroke of path ' + (i + 1));
+        assert.equal(actualNodeShape.attr("d"), translateDAttribute(expectedNodeShape.attr("d"), xoffs, yoffs), 'd of path ' + (i + 1));
     });
-    test.equal(actualNodeText.attr("text-anchor"), expectedNodeText.attr("text-anchor"), 'text-anchor of text');
-    test.equal(+actualNodeText.attr("x") - xoffs, +expectedNodeText.attr("x"), 'x of text');
-    test.equal(+actualNodeText.attr("y") - yoffs, +expectedNodeText.attr("y"), 'y of text');
-    test.equal(actualNodeText.attr("font-family"), expectedNodeText.attr("font-family"), 'font-family of text');
-    test.equal(actualNodeText.attr("font-size"), expectedNodeText.attr("font-size"), 'font-size of text');
-    test.equal(actualNodeText.attr("fill"), expectedNodeText.attr("fill"), 'fill of text');
+    assert.equal(actualNodeText.attr("text-anchor"), expectedNodeText.attr("text-anchor"), 'text-anchor of text');
+    assert.equal(+actualNodeText.attr("x") - xoffs, +expectedNodeText.attr("x"), 'x of text');
+    assert.equal(+actualNodeText.attr("y") - yoffs, +expectedNodeText.attr("y"), 'y of text');
+    assert.equal(actualNodeText.attr("font-family"), expectedNodeText.attr("font-family"), 'font-family of text');
+    assert.equal(actualNodeText.attr("font-size"), expectedNodeText.attr("font-size"), 'font-size of text');
+    assert.equal(actualNodeText.attr("fill"), expectedNodeText.attr("fill"), 'fill of text');
 
-    test.equal(actualNodeText.text(), expectedNodeText.text(), 'text of node group');
+    assert.equal(actualNodeText.text(), expectedNodeText.text(), 'text of node group');
 
     var actualNodeGroupDatum = actualNodeGroup.datum();
     var expectedNodeGroupDatum = expectedNodeGroup.datum();
     delete expectedNodeGroupDatum.parent;
     deepEqualData(actualNodeGroupDatum, expectedNodeGroupDatum, 'data of drawn node of shape equals Graphviz generated data');
 
-    test.end()
 });
