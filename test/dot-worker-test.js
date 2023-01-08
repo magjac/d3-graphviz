65076de3a597e69a8dedc21d7de63ec6eada90d9
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 15:27:00 +0100

    ensure that worker is terminated when test/dot-worker-test.js fails
    This currently works anyway, but an upcoming commit will remove the
    tape shim that takes care of this, so it must be done explicitly.

--- test/dot-worker-test.js
+++ test/dot-worker-test.js
@@ -4,71 +4,77 @@ import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import Worker from "tiny-worker";
 
-tape("dot() performs layout in a web worker in the background.", async function (test) {
+describe("dot()", () => {
+    let graphviz;
 
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-
-    var document = global.document = window.document;
-    var Blob = global.Blob = function (jsarray) {
-        return new Function(jsarray[0]);
-    }
-    var createObjectURL = window.URL.createObjectURL = function (js) {
-        return js;
-    }
-    global.Worker = Worker;
-
-    var graphviz = d3_graphviz.graphviz("#graph");
-
-    await new Promise(resolve => {
-        graphviz
-            .on("initEnd", resolve);
+    afterEach(() => {
+        graphviz._worker.terminate();
+        global.Worker = undefined;
     });
 
-    test.equal(d3.select('#graph').datum(), undefined, 'No data is attached before calling dot');
+    tape("dot() performs layout in a web worker in the background.", async function (test) {
 
-    graphviz
-        .tweenShapes(false)
-        .zoom(false)
-        .dot('digraph {a -> b; c}')
+        var window = global.window = jsdom(
+            `
+                <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+                <div id="graph"></div>
+                `,
+        );
 
-    test.equal(d3.select('#graph').datum(), undefined, 'No data is attached immediately after calling dot when worker is used');
+        var document = global.document = window.document;
+        var Blob = global.Blob = function (jsarray) {
+            return new Function(jsarray[0]);
+        }
+        var createObjectURL = window.URL.createObjectURL = function (js) {
+            return js;
+        }
+        global.Worker = Worker;
 
-    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph");
+
+        await new Promise(resolve => {
+            graphviz
+                .on("initEnd", resolve);
+        });
+
+        test.equal(d3.select('#graph').datum(), undefined, 'No data is attached before calling dot');
+
         graphviz
-            .render(resolve);
+            .tweenShapes(false)
+            .zoom(false)
+            .dot('digraph {a -> b; c}')
+
+        test.equal(d3.select('#graph').datum(), undefined, 'No data is attached immediately after calling dot when worker is used');
+
+        await new Promise(resolve => {
+            graphviz
+                .render(resolve);
+        });
+
+        test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
+        test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
+        test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
+        test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
+        test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
+
+        const err = await new Promise(resolve => {
+            graphviz
+                .onerror(resolve)
+                .dot('bad dot 1', callbackThatShouldNotBeCalled)
+                .render(callbackThatShouldNotBeCalled);
+        });
+
+        test.equal(
+            err,
+            "syntax error in line 1 near 'bad'\n",
+            'A registered error handler catches syntax errors in the dot source thrown during layout'
+        );
+
+        function callbackThatShouldNotBeCalled() {
+            test.error('Callback should not be called when an error occurs');
+        }
+
+
+        test.end();
     });
-
-    test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
-    test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-    test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-    test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
-    test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
-
-    const err = await new Promise(resolve => {
-        graphviz
-            .onerror(resolve)
-            .dot('bad dot 1', callbackThatShouldNotBeCalled)
-            .render(callbackThatShouldNotBeCalled);
-    });
-
-    graphviz._worker.terminate();
-    global.Worker = undefined;
-
-    test.equal(
-        err,
-        "syntax error in line 1 near 'bad'\n",
-        'A registered error handler catches syntax errors in the dot source thrown during layout'
-    );
-
-    function callbackThatShouldNotBeCalled() {
-        test.error('Callback should not be called when an error occurs');
-    }
-
-
-    test.end();
 });
