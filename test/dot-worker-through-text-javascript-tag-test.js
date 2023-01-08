2d87d4effb2d8869921325bce08bc118d21cd3e3
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 15:50:08 +0100

    ensure that worker is terminated when test/dot-worker-through-text-javascript-tag-test.js fails
    This currently works anyway, but an upcoming commit will remove the
    tape shim that takes care of this, so it must be done explicitly.

--- test/dot-worker-through-text-javascript-tag-test.js
+++ test/dot-worker-through-text-javascript-tag-test.js
@@ -4,49 +4,57 @@ import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import Worker from "tiny-worker";
 
-tape("dot() performs layout in a web worker in the background with text/javascript tag.", async function (test) {
+describe("dot()", () => {
+    let graphviz;
 
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="text/javascript"></script>
-            <div id="graph"></div>
-            `,
-    );
-    var document = global.document = window.document;
-    var Blob = global.Blob = function (jsarray) {
-        return new Function(jsarray[0]);
-    }
-    var createObjectURL = window.URL.createObjectURL = function (js) {
-        return js;
-    }
-    global.Worker = Worker;
+    afterEach(() => {
+        graphviz._worker.terminate();
+        global.Worker = undefined;
+    });
 
-    var graphviz = d3_graphviz.graphviz("#graph");
+    tape("dot() performs layout in a web worker in the background with text/javascript tag.", async function (test) {
 
-    await new Promise(resolve => {
+        var window = global.window = jsdom(
+            `
+                <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="text/javascript"></script>
+                <div id="graph"></div>
+                `,
+        );
+        var document = global.document = window.document;
+        var Blob = global.Blob = function (jsarray) {
+            return new Function(jsarray[0]);
+        }
+        var createObjectURL = window.URL.createObjectURL = function (js) {
+            return js;
+        }
+        global.Worker = Worker;
+
+        graphviz = d3_graphviz.graphviz("#graph");
+
+        await new Promise(resolve => {
             graphviz
                 .on("initEnd", resolve);
         });
 
-    test.equal(d3.select('#graph').datum(), undefined, 'No data is attached before calling dot');
+        test.equal(d3.select('#graph').datum(), undefined, 'No data is attached before calling dot');
 
-    await new Promise(resolve => {
-        graphviz
-            .tweenShapes(false)
-            .zoom(false)
+        await new Promise(resolve => {
+            graphviz
+                .tweenShapes(false)
+                .zoom(false)
                 .dot('digraph {a -> b; c}')
                 .render(resolve);
         });
 
-    test.equal(d3.select('#graph').datum(), undefined, 'No data is attached immediately after calling dot when worker is used');
+        test.equal(d3.select('#graph').datum(), undefined, 'No data is attached immediately after calling dot when worker is used');
 
-    test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
-    test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-    test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-    test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
-    test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
+        test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
+        test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
+        test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
+        test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
+        test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
 
-    await new Promise(resolve => {
+        await new Promise(resolve => {
             graphviz
                 .onerror((err) => {
                     test.equal(
@@ -60,11 +68,10 @@ tape("dot() performs layout in a web worker in the background with text/javascri
                 .render(callbackThatShouldNotBeCalled);
         });
 
-    function callbackThatShouldNotBeCalled() {
+        function callbackThatShouldNotBeCalled() {
             test.fail('Callback should not be called when an error occurs');
         }
-    graphviz._worker.terminate();
-    global.Worker = undefined;
 
-    test.end();
+        test.end();
+    });
 });
