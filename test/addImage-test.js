43a21ecd0680c795ffa386409d9dbf1218135de8
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 12:40:11 +0100

    refactor test/addImage-test.js to use await promise

--- test/addImage-test.js
+++ test/addImage-test.js
@@ -12,22 +12,21 @@ tape("graphviz().addImage() adds images to use in graph.", async function (test)
         graphviz = d3_graphviz.graphviz("#graph")
             .on("initEnd", resolve);
     });
-    startTest();
 
-    function startTest() {
+    await new Promise(resolve => {
         graphviz
             .zoom(false)
             .addImage("images/first.png", "400px", "300px")
             .addImage("images/second.png", "400px", "300px")
-            .renderDot('digraph { a[image="images/first.png"]; b[image="images/second.png"]; a -> b }');
+            .renderDot('digraph { a[image="images/first.png"]; b[image="images/second.png"]; a -> b }', resolve);
+    });
 
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
-        test.equal(d3.selectAll('image').size(), 2, 'Number of images');
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
+    test.equal(d3.selectAll('image').size(), 2, 'Number of images');
 
-        test.end();
-    }
+    test.end()
 });
