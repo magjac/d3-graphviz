45fc06733bb9b25c131739b4b56ad180a3130ad4
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 13:06:56 +0100

    refactor test/data-test.js to directly use assert instead of tape shim

--- test/data-test.js
+++ test/data-test.js
@@ -1,9 +1,10 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape("data() returns the data saved by dot().", async function (test) {
+it("data() returns the data saved by dot().", async () => {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
 
@@ -16,20 +17,19 @@ tape("data() returns the data saved by dot().", async function (test) {
     var savedData;
 
     var noData = graphviz.data();
-    test.equal(noData, null, 'Data is not avaliable before calling dot()');
+    assert.equal(noData, null, 'Data is not avaliable before calling dot()');
 
     await new Promise(resolve => {
         graphviz
             .dot('digraph {a -> b;}', () => {
                 savedData = graphviz.data();
-                test.notEqual(savedData, null, 'Data is avaliable after calling dot()');
+                assert.notEqual(savedData, null, 'Data is avaliable after calling dot()');
             })
             .render(resolve);
     });
 
     savedData = graphviz.data();
-    test.notEqual(savedData, null, 'Data is still avaliable after calling render()');
-    test.equal(savedData, d3.select("#graph").selectAll("svg").datum(), 'The retrieved data is the same as the data attached to the #graph element');
+    assert.notEqual(savedData, null, 'Data is still avaliable after calling render()');
+    assert.equal(savedData, d3.select("#graph").selectAll("svg").datum(), 'The retrieved data is the same as the data attached to the #graph element');
 
-    test.end();
 });
