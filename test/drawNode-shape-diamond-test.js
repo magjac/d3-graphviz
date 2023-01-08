3b8694cd0824df732fa35589a11c82d1abf2c7a2
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-08 17:02:59 +0100

    refactor test/drawNode-shape-diamond-test.js to directly use assert instead of tape shim

--- test/drawNode-shape-diamond-test.js
+++ test/drawNode-shape-diamond-test.js
@@ -1,11 +1,12 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import deepEqualData from "./deepEqualData.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 import {translatePointsAttribute} from "../src/svg.js"
 
-tape("Verify that diamond shape is drawn exactly as Graphviz does.", async function (test) {
+it("Verify that diamond shape is drawn exactly as Graphviz does.", async () => {
     var window = global.window = jsdom('<div id="expected-graph"></div><div id="actual-graph"></div>');
     var document = global.document = window.document;
     var expectedGraph = d3.select("#expected-graph");
@@ -56,27 +57,26 @@ tape("Verify that diamond shape is drawn exactly as Graphviz does.", async funct
     var xoffs = x - bbox.cx;
     var yoffs = y - bbox.cy;
 
-    test.equal(actualNodeGroup.attr("id"), expectedNodeGroup.attr("id"), 'id of group');
+    assert.equal(actualNodeGroup.attr("id"), expectedNodeGroup.attr("id"), 'id of group');
 
-    test.equal(actualNodeTitle.text(), expectedNodeTitle.text(), 'text of title');
+    assert.equal(actualNodeTitle.text(), expectedNodeTitle.text(), 'text of title');
 
-    test.equal(actualNodeShape.attr("fill"), expectedNodeShape.attr("fill"), 'fill of polygon');
-    test.equal(actualNodeShape.attr("stroke"), expectedNodeShape.attr("stroke"), 'stroke of polygon');
-    test.equal(actualNodeShape.attr("points"), translatePointsAttribute(expectedNodeShape.attr("points"), xoffs, yoffs), 'points of polygon');
+    assert.equal(actualNodeShape.attr("fill"), expectedNodeShape.attr("fill"), 'fill of polygon');
+    assert.equal(actualNodeShape.attr("stroke"), expectedNodeShape.attr("stroke"), 'stroke of polygon');
+    assert.equal(actualNodeShape.attr("points"), translatePointsAttribute(expectedNodeShape.attr("points"), xoffs, yoffs), 'points of polygon');
 
-    test.equal(actualNodeText.attr("text-anchor"), expectedNodeText.attr("text-anchor"), 'text-anchor of text');
-    test.equal(+actualNodeText.attr("x"), +expectedNodeText.attr("x") + xoffs, 'x of text');
-    test.equal(+actualNodeText.attr("y"), +expectedNodeText.attr("y") + yoffs, 'y of text');
-    test.equal(actualNodeText.attr("font-family"), expectedNodeText.attr("font-family"), 'font-family of text');
-    test.equal(actualNodeText.attr("font-size"), expectedNodeText.attr("font-size"), 'font-size of text');
-    test.equal(actualNodeText.attr("fill"), expectedNodeText.attr("fill"), 'fill of text');
+    assert.equal(actualNodeText.attr("text-anchor"), expectedNodeText.attr("text-anchor"), 'text-anchor of text');
+    assert.equal(+actualNodeText.attr("x"), +expectedNodeText.attr("x") + xoffs, 'x of text');
+    assert.equal(+actualNodeText.attr("y"), +expectedNodeText.attr("y") + yoffs, 'y of text');
+    assert.equal(actualNodeText.attr("font-family"), expectedNodeText.attr("font-family"), 'font-family of text');
+    assert.equal(actualNodeText.attr("font-size"), expectedNodeText.attr("font-size"), 'font-size of text');
+    assert.equal(actualNodeText.attr("fill"), expectedNodeText.attr("fill"), 'fill of text');
 
-    test.equal(actualNodeText.text(), expectedNodeText.text(), 'text of node group');
+    assert.equal(actualNodeText.text(), expectedNodeText.text(), 'text of node group');
 
     var actualNodeGroupDatum = actualNodeGroup.datum();
     var expectedNodeGroupDatum = expectedNodeGroup.datum();
     delete expectedNodeGroupDatum.parent;
     deepEqualData(actualNodeGroupDatum, expectedNodeGroupDatum, 'data of drawn node of shape equals Graphviz generated data');
 
-    test.end()
 });
