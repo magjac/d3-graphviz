0769b5eb289087432db150342b2c4367b77e6750
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-14 16:28:55 +0100

    refactor test/graphviz-options-test.js to directly use assert instead of tape shim

--- test/graphviz-options-test.js
+++ test/graphviz-options-test.js
@@ -1,9 +1,10 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().options() gets options.", function(test) {
+it("graphviz().options() gets options.", async () => {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
     var defaultOptions = {
@@ -12,13 +13,12 @@ tape("graphviz().options() gets options.", function(test) {
     };
     var graphviz = d3_graphviz.graphviz("#graph");
     var getOptions = graphviz.options();
-    test.equal(getOptions.tweenShapes, defaultOptions.tweenShapes, 'tweenShapes option set true by default');
-    test.equal(getOptions.totalMemory, defaultOptions.totalMemory, 'totalMemory option is undefined by default');
+    assert.equal(getOptions.tweenShapes, defaultOptions.tweenShapes, 'tweenShapes option set true by default');
+    assert.equal(getOptions.totalMemory, defaultOptions.totalMemory, 'totalMemory option is undefined by default');
 
-    test.end();
 });
 
-tape("graphviz(options) sets options at initialization.", function(test) {
+it("graphviz(options) sets options at initialization.", async () => {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
     var setOptions = {
@@ -27,13 +27,12 @@ tape("graphviz(options) sets options at initialization.", function(test) {
     };
     var graphviz = d3_graphviz.graphviz("#graph", setOptions);
     var getOptions = graphviz.options();
-    test.equal(getOptions.tweenShapes, setOptions.tweenShapes, 'tweenShapes option set false at initialization');
-    test.equal(getOptions.totalMemory, setOptions.totalMemory, 'totalMemory option set to 65536 at initialization');
+    assert.equal(getOptions.tweenShapes, setOptions.tweenShapes, 'tweenShapes option set false at initialization');
+    assert.equal(getOptions.totalMemory, setOptions.totalMemory, 'totalMemory option set to 65536 at initialization');
 
-    test.end();
 });
 
-tape("graphviz().options(options) sets options.", function(test) {
+it("graphviz().options(options) sets options.", async () => {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
     var setOptions1 = {
@@ -43,15 +42,14 @@ tape("graphviz().options(options) sets options.", function(test) {
     var graphviz = d3_graphviz.graphviz("#graph");
     graphviz.options(setOptions1);
     var getOptions1 = graphviz.options();
-    test.equal(getOptions1.tweenShapes, setOptions1.tweenShapes, 'tweenShapes option set false by graphviz.options()');
-    test.equal(getOptions1.totalMemory, setOptions1.totalMemory, 'totalMemory option set to 255 by graphviz.options()');
+    assert.equal(getOptions1.tweenShapes, setOptions1.tweenShapes, 'tweenShapes option set false by graphviz.options()');
+    assert.equal(getOptions1.totalMemory, setOptions1.totalMemory, 'totalMemory option set to 255 by graphviz.options()');
 
     var setOptions2 = {totalMemory: 32768};
     graphviz.options(setOptions2);
-    test.equal(getOptions1.totalMemory, setOptions1.totalMemory, 'A previously fetched option does not change when a new values is set');
+    assert.equal(getOptions1.totalMemory, setOptions1.totalMemory, 'A previously fetched option does not change when a new values is set');
     var getOptions2 = graphviz.options();
-    test.equal(getOptions2.tweenShapes, setOptions1.tweenShapes, 'tweenShapes option keeps the previous value because it is not specified in the options set');
-    test.equal(getOptions2.totalMemory, setOptions2.totalMemory, 'totalMemory option set to 32768 by graphviz.options()');
+    assert.equal(getOptions2.tweenShapes, setOptions1.tweenShapes, 'tweenShapes option keeps the previous value because it is not specified in the options set');
+    assert.equal(getOptions2.totalMemory, setOptions2.totalMemory, 'totalMemory option set to 32768 by graphviz.options()');
 
-    test.end();
 });
