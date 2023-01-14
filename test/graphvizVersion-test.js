3741e14d64742158650a14913d9ec581f35662ab
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-14 16:45:48 +0100

    refactor test/graphvizVersion-test.js to use await promise

--- test/graphvizVersion-test.js
+++ test/graphvizVersion-test.js
@@ -2,25 +2,27 @@ import jsdom from "./jsdom.js";
 import tape from "./tape.js";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().graphvizVersion() return the Graphviz version.", function (test) {
+tape("graphviz().graphvizVersion() return the Graphviz version.", async function (test) {
     global.window = jsdom('<div id="graph"></div>');
     global.document = window.document;
 
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on("initEnd", startTest);
+    var graphviz;
 
-    function startTest() {
-        const version = graphviz.graphvizVersion();
-        if (version == undefined) {
-            test.fail("version is not defined")
-        }
-        else {
-            const [major, minor, patch] = version.split('.');
-            test.ok(!isNaN(major), 'Major version number is a number');
-            test.ok(!isNaN(minor), 'Minor version number is a number');
-            test.ok(!isNaN(patch), 'Patch version number is a number');
-        }
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .on("initEnd", resolve);
+    });
 
-        test.end();
+    const version = graphviz.graphvizVersion();
+    if (version == undefined) {
+        test.fail("version is not defined")
     }
+    else {
+        const [major, minor, patch] = version.split('.');
+        test.ok(!isNaN(major), 'Major version number is a number');
+        test.ok(!isNaN(minor), 'Minor version number is a number');
+        test.ok(!isNaN(patch), 'Patch version number is a number');
+    }
+
+    test.end();
 });
