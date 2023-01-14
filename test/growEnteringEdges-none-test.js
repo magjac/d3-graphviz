f234f69f029a58934d2c3cee62d39b31521f661b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-14 16:48:08 +0100

    refactor test/growEnteringEdges-none-test.js to use await promise

--- test/growEnteringEdges-none-test.js
+++ test/growEnteringEdges-none-test.js
@@ -3,29 +3,39 @@ import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().render() renders growing edges from nodes with shape none.", function(test) {
+tape("graphviz().render() renders growing edges from nodes with shape none.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on("initEnd", startTest);
 
-    function startTest() {
+    var graphviz;
+
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .on("initEnd", resolve);
+    });
+
+    await new Promise(resolve => {
         graphviz
             .tweenShapes(false)
             .growEnteringEdges(true)
             .zoom(false)
-            .renderDot('digraph {a [shape="none"]; b}');
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of initial nodes');
-        test.equal(d3.selectAll('.edge').size(), 0, 'Number of initial edges');
-        test.equal(d3.selectAll('g').size(), 3, 'Number of groups');
-        test.equal(d3.selectAll('path').size(), 0, 'Number of paths');
-        graphviz
-            .renderDot('digraph {a [shape="none"]; b; a -> b}');
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after add');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges after add');
-        test.equal(d3.selectAll('g').size(), 4, 'Number of groups');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+            .renderDot('digraph {a [shape="none"]; b}', resolve);
+    });
 
-        test.end();
-    }
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of initial nodes');
+    test.equal(d3.selectAll('.edge').size(), 0, 'Number of initial edges');
+    test.equal(d3.selectAll('g').size(), 3, 'Number of groups');
+    test.equal(d3.selectAll('path').size(), 0, 'Number of paths');
+
+    await new Promise(resolve => {
+        graphviz
+            .renderDot('digraph {a [shape="none"]; b; a -> b}', resolve);
+    });
+
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after add');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges after add');
+    test.equal(d3.selectAll('g').size(), 4, 'Number of groups');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+
+    test.end();
 });
