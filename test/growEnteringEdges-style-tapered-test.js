9338dad44319822796a906cfb24ad5d42be6f1d4
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-14 16:48:59 +0100

    refactor test/growEnteringEdges-style-tapered-test.js to use await promise

--- test/growEnteringEdges-style-tapered-test.js
+++ test/growEnteringEdges-style-tapered-test.js
@@ -3,20 +3,23 @@ import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "../index.js";
 
-tape("Simple rendering an SVG from graphviz DOT.", function(test) {
+tape("Simple rendering an SVG from graphviz DOT.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .growEnteringEdges(true)
-        .renderDot('digraph {a -> b [style=tapered]}', checkGraph);
 
-    function checkGraph() {
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons');
-        test.equal(d3.selectAll('path').size(), 0, 'Number of paths');
+    var graphviz;
 
-        test.end();
-    }
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .growEnteringEdges(true)
+            .renderDot('digraph {a -> b [style=tapered]}', resolve);
+    });
+
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 0, 'Number of paths');
+
+    test.end();
 });
