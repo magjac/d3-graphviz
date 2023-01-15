e7c0ac8638b23d0d711edc84ef61f4e6823d3262
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 16:08:47 +0100

    refactor test/tweenPaths-test.js to directly use assert instead of tape shim

--- test/tweenPaths-test.js
+++ test/tweenPaths-test.js
@@ -1,10 +1,11 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_transition from "d3-transition";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().tweenPaths(true) enables path tweening during transitions.", async function (test) {
+it("graphviz().tweenPaths(true) enables path tweening during transitions.", async () => {
 
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
@@ -25,11 +26,11 @@ tape("graphviz().tweenPaths(true) enables path tweening during transitions.", as
         .dot('digraph {a -> b; c}')
         .render();
 
-    test.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
-    test.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
-    test.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
-    test.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
-    test.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
+    assert.equal(d3.selectAll('.node').size(), 3, 'Number of initial nodes');
+    assert.equal(d3.selectAll('.edge').size(), 1, 'Number of initial edges');
+    assert.equal(d3.selectAll('polygon').size(), 2, 'Number of initial polygons');
+    assert.equal(d3.selectAll('ellipse').size(), 3, 'Number of initial ellipses');
+    assert.equal(d3.selectAll('path').size(), 1, 'Number of initial paths');
     const transition1 = d3_transition.transition().duration(100);
 
     await new Promise(resolve => {
@@ -41,18 +42,17 @@ tape("graphviz().tweenPaths(true) enables path tweening during transitions.", as
             .render()
             .on("end", resolve);
 
-        test.equal(d3.selectAll('.node').size(), 3, 'Number of nodes immediately after rendering');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
-        test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons immediately after rendering');
-        test.equal(d3.selectAll('ellipse').size(), 3, 'Number of ellipses immediately after rendering');
-        test.equal(d3.selectAll('path').size(), 2, 'Number of paths immediately after rendering');
+        assert.equal(d3.selectAll('.node').size(), 3, 'Number of nodes immediately after rendering');
+        assert.equal(d3.selectAll('.edge').size(), 1, 'Number of edges immediately after rendering');
+        assert.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons immediately after rendering');
+        assert.equal(d3.selectAll('ellipse').size(), 3, 'Number of ellipses immediately after rendering');
+        assert.equal(d3.selectAll('path').size(), 2, 'Number of paths immediately after rendering');
     });
 
-    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after transition');
-    test.equal(d3.selectAll('.edge').size(), 2, 'Number of edges after transition');
-    test.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after transition');
-    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses after transition');
-    test.equal(d3.selectAll('path').size(), 2, 'Number of paths after transition');
+    assert.equal(d3.selectAll('.node').size(), 2, 'Number of nodes after transition');
+    assert.equal(d3.selectAll('.edge').size(), 2, 'Number of edges after transition');
+    assert.equal(d3.selectAll('polygon').size(), 3, 'Number of polygons after transition');
+    assert.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses after transition');
+    assert.equal(d3.selectAll('path').size(), 2, 'Number of paths after transition');
 
-    test.end();
 });
