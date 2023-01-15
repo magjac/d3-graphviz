edf261d25a41e184076d6f7289caa499b90613bf
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 15:49:02 +0100

    refactor test/logEvents-worker-test.js to use await promise

--- test/logEvents-worker-test.js
+++ test/logEvents-worker-test.js
@@ -12,7 +12,7 @@ describe("logEvents())", () => {
         global.Worker = undefined;
     });
 
-    tape("logEvents enables event logging using web worker.", function (test) {
+    tape("logEvents enables event logging using web worker.", async function (test) {
         var window = global.window = jsdom(
             `
                 <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
@@ -28,25 +28,28 @@ describe("logEvents())", () => {
         }
         global.Worker = Worker;
 
-        graphviz = d3_graphviz.graphviz("#graph")
-            .zoom(false)
-            .logEvents(true)
-            .renderDot('digraph {a -> b}')
-            .on("initEnd", checkEventLogging);
+        await new Promise(resolve => {
+            graphviz = d3_graphviz.graphviz("#graph")
+                .zoom(false)
+                .logEvents(true)
+                .renderDot('digraph {a -> b}')
+                .on("initEnd", resolve);
+        });
 
-        function checkEventLogging() {
-            var eventTypes = graphviz._eventTypes;
-            var n = 0;
-            for (let i in eventTypes) {
-                let eventType = eventTypes[i];
-                test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
-                n += 1;
-            }
-            test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
-            test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
-            graphviz.on("end", () => {
-                test.end();
-            });
+        var eventTypes = graphviz._eventTypes;
+        var n = 0;
+        for (let i in eventTypes) {
+            let eventType = eventTypes[i];
+            test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
+            n += 1;
         }
+        test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
+        test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
+
+        await new Promise(resolve => {
+            graphviz.on("end", resolve);
+        });
+
+        test.end();
     });
 });
