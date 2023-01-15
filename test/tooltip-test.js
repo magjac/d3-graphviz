1d75e8d01ba52872d382f61dc977c85d902c7031
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 13:58:14 +0100

    refactor test/tooltip-test.js to use await promise

--- test/tooltip-test.js
+++ test/tooltip-test.js
@@ -4,11 +4,15 @@ import * as d3 from "d3-selection";
 import * as d3_transition from "d3-transition";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz.renderDot() generates a correct SVG from graphviz DOT with graph tooltip.", function(test) {
+tape("graphviz.renderDot() generates a correct SVG from graphviz DOT with graph tooltip.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', startTest);
+    var graphviz;
+
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .on('initEnd', resolve);
+    });
 
     const svgDoc = `<svg width="8pt" height="8pt" viewBox="0.00 0.00 8.00 8.00" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g id="graph0" class="graph" transform="scale(1 1) rotate(0) translate(4 4)">
@@ -19,23 +23,26 @@ tape("graphviz.renderDot() generates a correct SVG from graphviz DOT with graph 
 </g>
 </svg>`;
 
-    function startTest() {
-        graphviz
-            .tweenShapes(false)
-            .zoom(false)
-            .renderDot('digraph {graph [tooltip="G"]}');
+    graphviz
+        .tweenShapes(false)
+        .zoom(false)
+        .renderDot('digraph {graph [tooltip="G"]}');
 
-        test.equal(d3.select('div').html(), svgDoc, "SVG after initial rendering");
+    test.equal(d3.select('div').html(), svgDoc, "SVG after initial rendering");
 
-        test.end();
-    }
+
+    test.end();
 });
 
-tape("graphviz.transition().renderDot() generates a correct SVG from graphviz DOT with graph tooltip.", function(test) {
+tape("graphviz.transition().renderDot() generates a correct SVG from graphviz DOT with graph tooltip.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on('initEnd', startTest);
+    var graphviz;
+
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .on('initEnd', resolve);
+    });
 
     const svgDoc = `<svg width="8pt" height="8pt" viewBox="0.00 0.00 8.00 8.00" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g id="graph0" class="graph" transform="scale(1 1) rotate(0) translate(4 4)">
@@ -46,31 +53,29 @@ tape("graphviz.transition().renderDot() generates a correct SVG from graphviz DO
 </g>
 </svg>`;
 
-    function startTest() {
-        var transition = d3_transition.transition().duration(0);
-        graphviz
-            .tweenShapes(false)
-            .tweenPaths(false)
-            .zoom(false)
-//            .fade(false)
-            .renderDot('digraph {graph [tooltip="G"]}');
+    var transition = d3_transition.transition().duration(0);
+    graphviz
+        .tweenShapes(false)
+        .tweenPaths(false)
+        .zoom(false)
+        .renderDot('digraph {graph [tooltip="G"]}');
 
-        test.equal(d3.select('div').html(), svgDoc, "SVG after initial rendering");
+    test.equal(d3.select('div').html(), svgDoc, "SVG after initial rendering");
 
-        graphviz
-            .tweenShapes(false)
-            .tweenPaths(false)
-            .zoom(false)
-//            .fade(false)
-            .transition(transition)
-            .renderDot('digraph {graph [tooltip="G"]}');
+    graphviz
+        .tweenShapes(false)
+        .tweenPaths(false)
+        .zoom(false)
+        .transition(transition)
+        .renderDot('digraph {graph [tooltip="G"]}');
 
+    await new Promise(resolve => {
         transition
             .transition()
-            .on("end", function() {
-                test.equal(d3.select('div').html(), svgDoc, "SVG after transition");
-                test.end();
-            });
-    }
+            .on("end", resolve);
+    });
 
+    test.equal(d3.select('div').html(), svgDoc, "SVG after transition");
+
+    test.end();
 });
