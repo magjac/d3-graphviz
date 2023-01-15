2cec02f7e25ec7da1688005042c3624a484e07de
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 13:56:17 +0100

    refactor test/simple-default-export-test.js to use await promise

--- test/simple-default-export-test.js
+++ test/simple-default-export-test.js
@@ -3,19 +3,22 @@ import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_graphviz from "d3-graphviz";
 
-tape("Simple rendering an SVG from graphviz DOT using default export", function (test) {
+tape("Simple rendering an SVG from graphviz DOT using default export", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .renderDot('digraph {a -> b;}', checkGraph);
 
-    function checkGraph() {
-        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-        test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+    var graphviz;
 
-        test.end();
-    }
+    await new Promise(resolve => {
+        graphviz = d3_graphviz.graphviz("#graph")
+            .renderDot('digraph {a -> b;}', resolve);
+    });
+
+    test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+    test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+    test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+    test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+    test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+
+    test.end();
 });
