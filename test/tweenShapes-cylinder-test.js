8f0aa02e1bf43cda8260de553b5fc1d5ad909740
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 14:03:17 +0100

    refactor test/tweenShapes-cylinder-test.js to use await promise

--- test/tweenShapes-cylinder-test.js
+++ test/tweenShapes-cylinder-test.js
@@ -4,45 +4,53 @@ import * as d3 from "d3-selection";
 import * as d3_transition from "d3-transition";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().tweenShapes() enables and disables shape tweening during transitions.", function(test) {
+tape("graphviz().tweenShapes() enables and disables shape tweening during transitions.", async function (test) {
 
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on("initEnd", startTest);
 
-    function startTest() {
+    var graphviz = d3_graphviz.graphviz("#graph");
+    await new Promise(resolve => {
+        graphviz
+            .on("initEnd", resolve);
+    });
+
+    await new Promise(resolve => {
         graphviz
             .transition(function() { return d3_transition.transition().duration(0)})
-            .renderDot('digraph {a}', function () {
-                test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes initially');
-                test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges initially');
-                test.equal(d3.selectAll('g').size(), 2, 'Number of groups initially');
-                test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses initially');
-                test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons initially');
-                test.equal(d3.selectAll('path').size(), 0, 'Number of paths initially');
+            .renderDot('digraph {a}', resolve);
+    });
 
-                graphviz
-                    .renderDot('digraph {a [shape="cylinder"]}', function () {
-                        test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes after shape change to cylinder');
-                        test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges after shape change to cylinder');
-                        test.equal(d3.selectAll('g').size(), 2, 'Number of groups after shape change to cylinder');
-                        test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after shape change to cylinder');
-                        test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons after shape change to cylinder');
-                        test.equal(d3.selectAll('path').size(), 2, 'Number of paths after shape change to cylinder');
+    test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes initially');
+    test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges initially');
+    test.equal(d3.selectAll('g').size(), 2, 'Number of groups initially');
+    test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses initially');
+    test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons initially');
+    test.equal(d3.selectAll('path').size(), 0, 'Number of paths initially');
 
-                        graphviz
-                            .renderDot('digraph {a}', function () {
-                                test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes after shape change from cylinder to default');
-                                test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges after shape change from cylinder to default');
-                                test.equal(d3.selectAll('g').size(), 2, 'Number of groups after shape change from cylinder to default');
-                                test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses after shape change from cylinder to default');
-                                test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons after shape change from cylinder to default');
-                                test.equal(d3.selectAll('path').size(), 0, 'Number of paths after shape change from cylinder to default');
+    await new Promise(resolve => {
+        graphviz
+                    .renderDot('digraph {a [shape="cylinder"]}', resolve);
+     });
 
-                                test.end();
-                            });
-                    });
-            });
-    }
+    test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes after shape change to cylinder');
+    test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges after shape change to cylinder');
+    test.equal(d3.selectAll('g').size(), 2, 'Number of groups after shape change to cylinder');
+    test.equal(d3.selectAll('ellipse').size(), 0, 'Number of ellipses after shape change to cylinder');
+    test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons after shape change to cylinder');
+    test.equal(d3.selectAll('path').size(), 2, 'Number of paths after shape change to cylinder');
+
+    await new Promise(resolve => {
+        graphviz
+            .renderDot('digraph {a}', resolve);
+    });
+
+    test.equal(d3.selectAll('.node').size(), 1, 'Number of nodes after shape change from cylinder to default');
+    test.equal(d3.selectAll('.edge').size(), 0, 'Number of edges after shape change from cylinder to default');
+    test.equal(d3.selectAll('g').size(), 2, 'Number of groups after shape change from cylinder to default');
+    test.equal(d3.selectAll('ellipse').size(), 1, 'Number of ellipses after shape change from cylinder to default');
+    test.equal(d3.selectAll('polygon').size(), 1, 'Number of polygons after shape change from cylinder to default');
+    test.equal(d3.selectAll('path').size(), 0, 'Number of paths after shape change from cylinder to default');
+
+    test.end();
 });
