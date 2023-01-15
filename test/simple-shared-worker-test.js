74fa7c71e464dd542ed593f8f0ccd59ea22a205d
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 15:50:43 +0100

    refactor test/simple-shared-worker-test.js to use await promise

--- test/simple-shared-worker-test.js
+++ test/simple-shared-worker-test.js
@@ -12,7 +12,7 @@ describe("renderDot()", () => {
         global.SharedWorker = undefined;
     });
 
-    tape("Simple rendering an SVG from graphviz DOT.", function (test) {
+    tape("Simple rendering an SVG from graphviz DOT.", async function (test) {
         var window = global.window = jsdom(
             `
                 <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
@@ -22,17 +22,18 @@ describe("renderDot()", () => {
         var document = global.document = window.document;
         global.SharedWorker = SharedWorker;
 
-        graphviz = d3_graphviz.graphviz("#graph", { useSharedWorker: true })
-            .renderDot('digraph {a -> b;}', checkGraph);
 
-        function checkGraph() {
-            test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
-            test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
-            test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
-            test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
-            test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+        await new Promise(resolve => {
+            graphviz = d3_graphviz.graphviz("#graph", { useSharedWorker: true })
+                .renderDot('digraph {a -> b;}', resolve);
+        });
 
-            test.end();
-        }
+        test.equal(d3.selectAll('.node').size(), 2, 'Number of nodes');
+        test.equal(d3.selectAll('.edge').size(), 1, 'Number of edges');
+        test.equal(d3.selectAll('ellipse').size(), 2, 'Number of ellipses');
+        test.equal(d3.selectAll('polygon').size(), 2, 'Number of polygons');
+        test.equal(d3.selectAll('path').size(), 1, 'Number of paths');
+
+        test.end();
     });
 });
