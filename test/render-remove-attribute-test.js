b2434c8c0d27c7a604e27289c9dd467b0084ec4b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 13:51:41 +0100

    refactor test/render-remove-attribute-test.js to directly use assert instead of tape shim

--- test/render-remove-attribute-test.js
+++ test/render-remove-attribute-test.js
@@ -1,10 +1,11 @@
-import tape from "./tape.js";
+import assert from "assert";
+import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
 import * as d3_transition from "d3-transition";
 import * as d3_graphviz from "../index.js";
 
-tape("graphviz().render() removes attribute from SVG element when attribute is removed from Graphviz node.", async function (test) {
+it("graphviz().render() removes attribute from SVG element when attribute is removed from Graphviz node.", async () => {
 
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
@@ -22,14 +23,13 @@ tape("graphviz().render() removes attribute from SVG element when attribute is r
             .renderDot('digraph {a [style=dashed]}', resolve);
     });
 
-    test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
+    assert.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), '5,2', 'stroke-dasharray is present when style="dashed"');
 
     await new Promise(resolve => {
         graphviz
             .renderDot('digraph {a}', resolve);
     });
 
-    test.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
+    assert.equal(d3.selectAll('ellipse').attr('stroke-dasharray'), null, 'stroke-dasharray not present when style is not used');
 
-    test.end();
 });
