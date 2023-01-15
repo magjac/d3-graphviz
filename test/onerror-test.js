87bc160139d5c5153f0b848cfb78c3df8bc473e3
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 13:50:58 +0100

    refactor test/onerror-test.js to use await promise

--- test/onerror-test.js
+++ test/onerror-test.js
@@ -2,52 +2,62 @@ import tape from "./tape.js";
 import jsdom from "./jsdom.js";
 import * as d3_graphviz from "../index.js";
 
-tape("onerror() registers dot layout error handler.", function(test) {
+tape("onerror() registers dot layout error handler.", async function (test) {
     var window = global.window = jsdom('<div id="graph"></div>');
     var document = global.document = window.document;
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .on("initEnd", startTest);
+    var graphviz = d3_graphviz.graphviz("#graph");
+
+    await new Promise(resolve => {
+        graphviz.on("initEnd", resolve);
+    });
 
     var errorsCaught = 0;
 
-    function startTest() {
+    let resolve;
+    const onErrorHandler = (err)=>{
+        errorsCaught += 1;
+        resolve(err);
+    };
+
+    const err = await new Promise(_resolve => {
+        resolve = _resolve;
         graphviz
             .zoom(false)
-            .onerror(handleError)
+            .onerror(onErrorHandler)
             .renderDot('{bad dot 1}');
-    }
+    });
 
-    function handleError(err) {
-        errorsCaught += 1;
-        test.equal(
-            err,
-            "syntax error in line 1 near '{'\n",
-            'A registered error handler catches syntax errors in the dot source thrown during layout ' + (errorsCaught == 1 ? 'the first' : 'a second') + 'time'
-        );
+    test.equal(
+        err,
+        "syntax error in line 1 near '{'\n",
+        'A registered error handler catches syntax errors in the dot source thrown during layout ' + (errorsCaught == 1 ? 'the first' : 'a second') + 'time'
+    );
+
+    await new Promise(_resolve => {
+        resolve = _resolve;
         if (errorsCaught == 1) {
             graphviz
-                .renderDot('{bad dot 2}', stage2);
+                .renderDot('{bad dot 2}', resolve);
         } else {
            graphviz
-                .renderDot('digraph {a -> b}', stage2);
+               .renderDot('digraph {a -> b}', resolve);
         }
-    }
+    });
 
-    function stage2(err) {
-        test.ok(errorsCaught <= 2, 'The error handler does not catch any errors in correct dot source');
-        test.ok(errorsCaught >= 2, 'The error handler catches errors also after already having caught errors once already');
+    test.ok(errorsCaught <= 2, 'The error handler does not catch any errors in correct dot source');
+    test.ok(errorsCaught >= 2, 'The error handler catches errors also after already having caught errors once already');
+    graphviz
+        .onerror(null);
+
+    function renderDot() {
         graphviz
-         .onerror(null);
-
-        function renderDot() {
-            graphviz
-                .renderDot('{bad dot 3}');
-        }
-
-        test.throws(renderDot, 'Without a registered error handler, errors in the dot source throws error');
-
-        test.equal(errorsCaught, 2, 'Without a registered error handler, errors in the dot source are not caught');
-
-        test.end();
+            .renderDot('{bad dot 3}');
     }
+
+    test.throws(renderDot, 'Without a registered error handler, errors in the dot source throws error');
+
+    test.equal(errorsCaught, 2, 'Without a registered error handler, errors in the dot source are not caught');
+
+
+    test.end();
 });
