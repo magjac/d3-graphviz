50e3be49d7347686091d97687bdaf8750fb9a49f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 14:35:52 +0100

    ensure that worker is terminated when test/graphvizVersion-worker-test.js fails
    This currently works anyway, but an upcoming commit will remove the
    tape shim that takes care of this, so it must be done explicitly.

--- test/graphvizVersion-worker-test.js
+++ test/graphvizVersion-worker-test.js
@@ -3,40 +3,46 @@ import jsdom from "./jsdom.js";
 import * as d3_graphviz from "../index.js";
 import Worker from "tiny-worker";
 
-tape("graphviz().graphvizVersion() returns the Graphviz version.", function (test) {
+describe("graphvizVersion()", () => {
+    let graphviz;
 
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-    var document = global.document = window.document;
-    var Blob = global.Blob = function (jsarray) {
-        return new Function(jsarray[0]);
-    }
-    var createObjectURL = window.URL.createObjectURL = function (js) {
-        return js;
-    }
-    global.Worker = Worker;
-
-    var graphviz = d3_graphviz.graphviz("#graph", { useWorker: true })
-        .on("initEnd", startTest);
-
-    function startTest() {
-        const version = graphviz.graphvizVersion();
-        if (version == undefined) {
-            test.fail("version is not defined")
-        }
-        else {
-            const [major, minor, patch] = version.split('.');
-            test.ok(!isNaN(major), 'Major version number is a number');
-            test.ok(!isNaN(minor), 'Minor version number is a number');
-            test.ok(!isNaN(patch), 'Patch version number is a number');
-        }
-
+    afterEach(() => {
         graphviz._worker.terminate();
         global.Worker = undefined;
-        test.end();
-    }
+    });
+
+    tape("graphviz().graphvizVersion() returns the Graphviz version.", function (test) {
+
+        var window = global.window = jsdom(
+            `
+                <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+                <div id="graph"></div>
+                `,
+        );
+        var document = global.document = window.document;
+        var Blob = global.Blob = function (jsarray) {
+            return new Function(jsarray[0]);
+        }
+        var createObjectURL = window.URL.createObjectURL = function (js) {
+            return js;
+        }
+        global.Worker = Worker;
+
+        graphviz = d3_graphviz.graphviz("#graph", { useWorker: true })
+            .on("initEnd", startTest);
+
+        function startTest() {
+            const version = graphviz.graphvizVersion();
+            if (version == undefined) {
+                test.fail("version is not defined")
+            }
+            else {
+                const [major, minor, patch] = version.split('.');
+                test.ok(!isNaN(major), 'Major version number is a number');
+                test.ok(!isNaN(minor), 'Minor version number is a number');
+                test.ok(!isNaN(patch), 'Patch version number is a number');
+            }
+            test.end();
+        }
+    });
 });
