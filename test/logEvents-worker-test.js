8be17e1d3a33537be2d25a253e5b81945b295220
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-15 14:35:52 +0100

    ensure that worker is terminated when test/logEvents-worker-test.js fails
    This currently works anyway, but an upcoming commit will remove the
    tape shim that takes care of this, so it must be done explicitly.

--- test/logEvents-worker-test.js
+++ test/logEvents-worker-test.js
@@ -4,42 +4,49 @@ import * as d3_graphviz from "../index.js";
 import * as d3_transition from "d3-transition";
 import Worker from "tiny-worker";
 
-tape("logEvents enables event logging using web worker.", function(test) {
-    var window = global.window = jsdom(
-        `
-            <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
-            <div id="graph"></div>
-            `,
-    );
-    global.document = window.document;
-    global.Blob = function (jsarray) {
-        return new Function(jsarray[0]);
-    }
-    window.URL.createObjectURL = function (js) {
-        return js;
-    }
-    global.Worker = Worker;
+describe("logEvents())", () => {
+    let graphviz;
 
-    var graphviz = d3_graphviz.graphviz("#graph")
-        .zoom(false)
-        .logEvents(true)
-        .renderDot('digraph {a -> b}')
-        .on("initEnd", checkEventLogging);
+    afterEach(() => {
+        graphviz._worker.terminate();
+        global.Worker = undefined;
+    });
 
-    function checkEventLogging() {
-        var eventTypes = graphviz._eventTypes;
-        var n = 0;
-        for (let i in eventTypes) {
-            let eventType = eventTypes[i];
-            test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
-            n += 1;
+    tape("logEvents enables event logging using web worker.", function (test) {
+        var window = global.window = jsdom(
+            `
+                <script src="http://dummyhost/test/@hpcc-js/wasm/dist/wrapper.js" type="javascript/worker"></script>
+                <div id="graph"></div>
+                `,
+        );
+        global.document = window.document;
+        global.Blob = function (jsarray) {
+            return new Function(jsarray[0]);
         }
-        test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
-        test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
-        graphviz.on("end", () => {
-            graphviz._worker.terminate();
-            global.Worker = undefined;
-            test.end();
-        });
-    }
+        window.URL.createObjectURL = function (js) {
+            return js;
+        }
+        global.Worker = Worker;
+
+        graphviz = d3_graphviz.graphviz("#graph")
+            .zoom(false)
+            .logEvents(true)
+            .renderDot('digraph {a -> b}')
+            .on("initEnd", checkEventLogging);
+
+        function checkEventLogging() {
+            var eventTypes = graphviz._eventTypes;
+            var n = 0;
+            for (let i in eventTypes) {
+                let eventType = eventTypes[i];
+                test.equal(typeof graphviz._dispatch.on(eventType + ".log"), 'function', "An event named " + eventType + ".log is registered when event logging is enabled");
+                n += 1;
+            }
+            test.ok(n > 10, "More than 10 events are registered when event logging is enabled");
+            test.equal(n, eventTypes.length, "All " + eventTypes.length + " events are registered when event logging is enabled");
+            graphviz.on("end", () => {
+                test.end();
+            });
+        }
+    });
 });
