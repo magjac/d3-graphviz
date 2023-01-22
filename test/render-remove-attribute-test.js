0e763b49f132b90e22475487ad3753c5d4455d19
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-01-22 10:44:17 +0100

    remove useless import in test/render-remove-attribute-test.js

--- test/render-remove-attribute-test.js
+++ test/render-remove-attribute-test.js
@@ -2,7 +2,6 @@ import assert from "assert";
 import it from "./it.js";
 import jsdom from "./jsdom.js";
 import * as d3 from "d3-selection";
-import * as d3_transition from "d3-transition";
 import * as d3_graphviz from "../index.js";
 
 it("graphviz().render() removes attribute from SVG element when attribute is removed from Graphviz node.", async () => {
